2631c080d5d8c0a19c513bab79e73c7a593da619
helenphillips <helen.r.phillips@googlemail.com>
2017-03-10 19:31:15 +0100

    I've done lots today, including creating basic plotting code and creating models

---
+++ 4_Modelling.R
@@ -0,0 +1,72 @@
+########################################################
+# 0. Set Working Directory
+########################################################
+
+if(Sys.info()["nodename"] == "IDIVNB193"){
+  setwd("C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\")
+}
+
+
+
+#################################################
+# 1. Loading libraries
+#################################################
+library(maptools)
+library(maps)
+library(lme4)
+#################################################
+# 2. Loading in variables
+#################################################
+
+data_in <-"3_Data"
+
+files <- list.files(file.path(data_in))
+file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
+file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
+
+file_dates <- as.Date(file_dates)
+date <- max(file_dates)
+loadin <- files[grep(date, files)]
+
+
+
+if(!dir.exists("Figures")){
+  dir.create("Figures")
+}
+figures <- "Figures"
+#################################################
+# 3. Load in data
+#################################################
+
+sites <- read.csv(file.path(data_in, loadin))
+
+
+
+hist(sites$ph_new)
+hist(poly(sites$ph_new, 2))
+
+#################################################
+# 4. Species Richness
+#################################################
+
+sites$scalePH <-scale(sites$ph_new)
+
+sites_habitat <- sites[sites$HabitatCover != "Unknown/Other",]
+
+## There are some habitat covers that are not suitable for modelling at this stage
+sites_habitat <- droplevels(sites_habitat[!(sites_habitat$HabitatCover %in% c("Cropland/Other vegetation mosaic", "Paddy field")),])
+
+sp_habitat <- glmer(NumberofSpecies ~ scalePH * HabitatCover + (1|Study_Name), data = sites_habitat, family = poisson)
+summary(sp_habitat)
+sp_habitat2 <- update(sp_habitat, .~. -scalePH:HabitatCover)
+anova(sp_habitat, sp_habitat2) ## Significant at 0.05 level
+plot(sp_habitat)
+
+
+
+
+sites_lu <- sites[sites$LandUse != "Unknown",]
+sp_lu <- glmer(NumberofSpecies ~ scalePH * LandUse + (1|Study_Name), data = sites_lu, family = poisson)
+summary(sp_lu)
+sp_lu2 <- update(sp_lu, .~. -scalePH:LandUse)
+anova(sp_lu, sp_lu2) ## Highly significant
---
+++ Functions/Plots.R
@@ -0,0 +1,105 @@
+Effect1 <- "scalePH"
+Effect2 <- "HabitatCover"
+data <- sites_habitat
+model <- sp_habitat
+
+responseVar <- "NumberofSpecies"
+
+
+plotInteraction <- function(model, Effect1, Effect2, responseVar, seMultiplier = 1.96, data, cols = "Black", legend.position = "topleft", ylabel = "", xlabel = ""){
+  # ci <- confint(model, method="Wald")
+  
+  ## Create values to predict over
+  if(is.numeric(data[,which(names(data) == Effect1)])){
+    eff1 <- seq(min(data[,which(names(data) == Effect1)], na.rm = TRUE), max(data[,which(names(data) == Effect1)], na.rm = TRUE), length.out = 100)
+  } else { eff1 <- levels(model@frame[,which(names(model@frame) == Effect1)])}
+  
+  if(is.numeric(data[,which(names(data) == Effect2)])){
+    eff2 <- seq(min(data[,which(names(data) == Effect2)], na.rm = TRUE), max(data[,which(names(data) == Effect2)], na.rm = TRUE), length.out = 100)
+  } else { eff2 <- levels(model@frame[,which(names(model@frame) == Effect2)])}
+  
+  newdata <- with(data, expand.grid(E1 = eff1, E2 = eff2))
+  names(newdata) <- c(Effect1, Effect2)
+  
+  ## Remove continuous variables not represented by data
+  if(is.numeric(data[,which(names(data) == Effect1)]) && !(is.numeric(data[,which(names(data) == Effect2)]))){
+    for(l in levels(newdata[,which(names(newdata) == Effect2)])){
+      tmp <- data[data[which(names(data) == Effect2)] == l,]
+      remove <- c(intersect(which(newdata[,1] < min(tmp$scalePH, na.rm = TRUE)), which(newdata[,2] == l)),
+                  intersect(which(newdata[,1] > max(tmp$scalePH, na.rm = TRUE)), which(newdata[,2] == l)))
+      newdata <- newdata[-remove,]
+    }
+  }
+  
+  if(is.numeric(data[,which(names(data) == Effect2)]) && !(is.numeric(data[,which(names(data) == Effect1)]))){
+    for(l in levels(newdata[,which(names(newdata) == Effect1)])){
+      tmp <- data[data[which(names(data) == Effect1)] == l,]
+      remove <- c(intersect(which(newdata[,1] < min(tmp$scalePH, na.rm = TRUE)), which(newdata[,2] == l)),
+                  intersect(which(newdata[,1] > max(tmp$scalePH, na.rm = TRUE)), which(newdata[,2] == l)))
+      newdata <- newdata[-remove,]
+    }
+  }
+  
+  newdata$Response <- predict(model,newdata, re.form = NA)
+  names(newdata)[which(names(newdata) == "Response")] <- responseVar
+  mm <- model.matrix(terms(model), newdata)
+  
+  coef.names<-names(fixef(model))
+  original.mm.names<-dimnames(mm)[[2]]
+  if(length(coef.names)!=length(original.mm.names)){mm<-mm[,dimnames(mm)[[2]]%in%coef.names]} 
+  
+  
+  pvar1 <- diag(mm %*% base::tcrossprod(as.matrix(vcov(model)), mm))
+  
+  newdata$upper <- newdata[,which(names(newdata) == responseVar)] + seMultiplier * sqrt(pvar1)
+  newdata$lower <- newdata[,which(names(newdata) == responseVar)] - seMultiplier * sqrt(pvar1)
+  
+  
+  if(length(which(c(class(newdata[,1]), class(newdata[,2])) == "factor")) != 1){
+    stop("Plotting only works for one factor level and one continuous at the moment. Sorry")
+  }
+  
+  
+  #######################################
+  ## One factor level and one continuous
+  ########################################
+  fac <- levels(newdata[,which(c(class(newdata[,1]), class(newdata[,2])) == "factor")])
+  f <- which(c(class(newdata[,1]), class(newdata[,2])) == "factor")
+  n <- which(c(class(newdata[,1]), class(newdata[,2])) == "numeric")
+  r <- which(names(newdata) == responseVar)
+  if(length(cols) != length(fac)){
+    print("The length of colours does not match the number of factor levels. Some may be removed or duplicated")
+  }
+  cols <- rep(cols, length.out = length(fac))
+  
+
+  
+  
+  plot(-1e+05, -1e+05, ylim = c(min(newdata$lower,na.rm = TRUE), max(newdata$upper, na.rm = TRUE)),
+       xlim = c(min(newdata[,n],na.rm = TRUE), max(newdata[,n], na.rm = TRUE)),  ylab = ylabel, xlab = xlabel)
+  
+  
+  X.Vec <- c(newdata[,n][newdata[,f] == fac[1]], max(newdata[,n][newdata[,f] == fac[1]]), 
+             rev(newdata[,n][newdata[,f] == fac[1]]), min(newdata[,n][newdata[,f] == fac[1]]))
+  Y.Vec <- c(newdata$lower[newdata[,f] == fac[1]], tail(newdata$upper[newdata[,f] == fac[1]], 1), rev(newdata$upper[newdata[,f] == fac[1]]), newdata$lower[newdata[,f] == fac[1]][1])
+  
+  polygon(X.Vec, Y.Vec, col = "grey", border = NA)
+  
+  
+  points(newdata[,n][newdata[,f] == fac[1]], newdata[,r][newdata[,f] == fac[1]], 
+              col = cols[1],  type = "l", lwd = 2)
+  
+  for(lev in 2:length(fac)){
+    
+    X.Vec <- c(newdata[,n][newdata[,f] == fac[lev]], max(newdata[,n][newdata[,f] == fac[lev]]), 
+               rev(newdata[,n][newdata[,f] == fac[lev]]), min(newdata[,n][newdata[,f] == fac[lev]]))
+    Y.Vec <- c(newdata$lower[newdata[,f] == fac[lev]], tail(newdata$upper[newdata[,f] == fac[lev]], 1), rev(newdata$upper[newdata[,f] == fac[lev]]), newdata$lower[newdata[,f] == fac[lev]][1])
+    
+    polygon(X.Vec, Y.Vec, col = "grey", border = NA)
+    
+    
+    points(newdata[,n][newdata[,f] == fac[lev]], newdata[,r][newdata[,f] == fac[lev]], 
+           type = "l", lwd = 2, col = cols[lev])
+  }
+  legend(legend.position, legend = fac, col = cols, lwd = 2, bty = "n")
+}
