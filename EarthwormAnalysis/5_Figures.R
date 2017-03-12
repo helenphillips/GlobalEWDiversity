6581e930fd71d2281793dbe043b7e8568b121bd3
helenphillips <helen.r.phillips@googlemail.com>
2017-03-12 23:12:40 +0100

    Rubbish code to quick plot main effects

--- EarthwormAnalysis/5_Figures.R
+++ EarthwormAnalysis/5_Figures.R
@@ -14,6 +14,9 @@ if(Sys.info()["nodename"] == "IDIVNB193"){
 source(file.path("Functions", "Plots.R"))
 source(file.path("Functions", "ColourPicker.R"))
 
+library(lme4)
+library(Hmisc)
+
 #################################################
 # 2. Loading in variables
 #################################################
@@ -43,6 +46,9 @@ figures <- "Figures"
 sites <- read.csv(file.path(data_in, loadin))
 rm(loadin)
 
+sites$scalePH <-scale(sites$ph_new)
+sites_habitat <- sites[sites$HabitatCover != "Unknown/Other",]
+sites_lu <- droplevels(sites[sites$LandUse != "Unknown",])
 
 #################################################
 # 4. Load in models
@@ -61,7 +67,7 @@ luCols <- ColourPicker(sites_lu$LandUse)
 #################################################
 # 6. Figures
 #################################################
-
+### Interactions
 plotInteraction(model = sp_habitat, Effect1 = "scalePH", Effect2 = "HabitatCover", 
                 responseVar = "NumberofSpecies", seMultiplier = 1.96, 
                 data = sites_habitat, cols = habitCols, legend.position = "topleft", 
@@ -71,4 +77,75 @@ plotInteraction(model = sp_lu, Effect1 = "scalePH", Effect2 = "LandUse",
                 responseVar = "NumberofSpecies", seMultiplier = 1.96, 
                 data = sites_lu, cols = luCols, legend.position = "bottomleft", 
                 ylabel = "", xlabel = "")
+
+
+
+### Main effects
+### Habitat cover
+newdata <- with(sp_habitat@frame, expand.grid(scalePH = mean(scalePH, na.rm = TRUE), HabitatCover=levels(HabitatCover)))
+newdata$NumberofSpecies <- predict(sp_habitat,newdata, re.form = NA)
+
+mm <- model.matrix(terms(sp_habitat), newdata)
+
+coef.names<-names(fixef(sp_habitat))
+original.mm.names<-dimnames(mm)[[2]]
+if(length(coef.names)!=length(original.mm.names)){mm<-mm[,dimnames(mm)[[2]]%in%coef.names]} 
+
+
+pvar1 <- diag(mm %*% base::tcrossprod(as.matrix(vcov(sp_habitat)), mm))
+seMultiplier <- 1.96
+newdata$upper <- newdata$NumberofSpecies + seMultiplier * sqrt(pvar1)
+newdata$lower <- newdata$NumberofSpecies - seMultiplier * sqrt(pvar1)
+
+
+newdata <- droplevels(newdata[newdata$HabitatCover != "Paddy field",])
+newdata$cols <- ColourPicker(newdata$HabitatCover)
+  newdata$cols2 <- paste("#", newdata$cols, sep="")
+
+par(xpd=TRUE)
+par(mar = c(3, 3, 2, 15))
+plot(1:nrow(newdata) -1e+05, ylim = c(min(newdata$lower,na.rm = TRUE), max(newdata$upper, na.rm = TRUE)),
+      ylab = "log-Number of species", xlab = "Habitat cover", xaxt='n')
+#xlim = c(min(newdata[,n],na.rm = TRUE), max(newdata[,n], na.rm = TRUE)), 
+
+errbar(1:nrow(newdata), newdata$NumberofSpecies, newdata$upper, newdata$lower,
+       add = TRUE, col = newdata$cols2, errbar.col = newdata$cols2, cex = 1.5)
+legend(8.5,2,legend = newdata$HabitatCover, col = newdata$cols2, pch = 19, bty = "n", cex = 1.1)
+
+# legend("bottomleft", legend = newdata$HabitatCover, col = newdata$cols2, lwd = 2, bty = "n")
+
+
+
+
+### Land use
+newdata <- with(sp_lu@frame, expand.grid(scalePH = mean(scalePH, na.rm = TRUE), LandUse=levels(LandUse)))
+newdata$NumberofSpecies <- predict(sp_lu,newdata, re.form = NA)
+
+mm <- model.matrix(terms(sp_lu), newdata)
+
+coef.names<-names(fixef(sp_lu))
+original.mm.names<-dimnames(mm)[[2]]
+if(length(coef.names)!=length(original.mm.names)){mm<-mm[,dimnames(mm)[[2]]%in%coef.names]} 
+
+
+pvar1 <- diag(mm %*% base::tcrossprod(as.matrix(vcov(sp_lu)), mm))
+seMultiplier <- 1.96
+newdata$upper <- newdata$NumberofSpecies + seMultiplier * sqrt(pvar1)
+newdata$lower <- newdata$NumberofSpecies - seMultiplier * sqrt(pvar1)
+
+newdata$cols <- ColourPicker(newdata$LandUse)
+newdata$cols2 <- paste("#", newdata$cols, sep="")
+
+par(xpd=TRUE)
+par(mar = c(3, 3, 2, 15))
+plot(1:nrow(newdata) -1e+05, ylim = c(min(newdata$lower,na.rm = TRUE), max(newdata$upper, na.rm = TRUE)),
+     ylab = "log-Number of species", xlab = "Habitat cover", xaxt='n')
+#xlim = c(min(newdata[,n],na.rm = TRUE), max(newdata[,n], na.rm = TRUE)), 
+
+errbar(1:nrow(newdata), newdata$NumberofSpecies, newdata$upper, newdata$lower,
+       add = TRUE, col = newdata$cols2, errbar.col = newdata$cols2, cex = 1.5)
+legend(6.3,1.5,legend = newdata$LandUse, col = newdata$cols2, pch = 19, bty = "n", cex = 1.1)
+
+# legend("bottomleft", legend = newdata$HabitatCover, col = newdata$cols2, lwd = 2, bty = "n")
+
 
