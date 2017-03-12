30df79b99bddf44ff4471043873074414cebfc7b
helenphillips <helen.r.phillips@googlemail.com>
2017-03-12 23:12:21 +0100

    Stuff I forgot to put in before

--- Functions/ColourPicker.R
+++ Functions/ColourPicker.R
@@ -9,7 +9,8 @@ ColourPicker <- function(variable){
                                  "Paddy field", "Sparse vegetation", "Shrub", "Wetland", "Unknown/Other"),
                      colour =c("4CAF50", "1B5E20","E65100", "FF9800","6D4C41","A1887F","A5D6A7","9C27B0",
                                 "F06292","26C6DA","0288D1","607D8B"))
-    colos <- tp$colour[which(levels(variable) == tp$habitat)]
+    colos <- tp$colour[match(levels(variable), tp$habitat)]
+    
     return(colos)
     }
   
@@ -19,7 +20,7 @@ ColourPicker <- function(variable){
     tp <- data.frame(habitat = c("Pasture","Primary vegetation", "Production - Arable","Production - Crop plantations",
                                  "Production - Wood plantation", "Secondary vegetation", "Unknown"),
                      colour =c("FFEB3B", "1B5E20", "E65100", "FF9800", "795548", "4CAF50", "607D8B"))
-    colos <- tp$colour[which(levels(variable) == tp$habitat)]
+    colos <- tp$colour[match(levels(variable), tp$habitat)]
     return(colos)
     
   }
