adb8a623185083254ce148bdcc329a6c21d05256
helenphillips <helen.r.phillips@googlemail.com>
2017-03-12 15:19:18 +0100

    Adding new colours

--- EarthwormAnalysis/Functions/ColourPicker.R
+++ EarthwormAnalysis/Functions/ColourPicker.R
@@ -1,13 +1,14 @@
-
 ColourPicker <- function(variable){
   
   if(any(levels(variable) %in% c("Broadleaf deciduous forest", "Herbaceous"))){
     colos <- c(rep(NA, length(levels(variable))))
     
-    tp <- data.frame(habitat = c("Broadleaf deciduous forest","Broadleaf evergreen forest","Cropland",                        
-                                 "Herbaceous","Herbaceous with spare tree/shrub", "Needleleaf evergreen forest"),
-                     colour =c("696346", "FFD300", "68089D", "103B9D", "0041D8", "1B1B1B"))
-                      # colour = c("red", "green", "blue", "grey", "purple", "black"))
+    tp <- data.frame(habitat = c("Broadleaf deciduous forest","Broadleaf evergreen forest","Cropland", 
+                                 "Cropland/Other vegetation mosaic",
+                                 "Herbaceous","Herbaceous with spare tree/shrub", "Needleleaf evergreen forest",
+                                 "Paddy field", "Sparse vegetation", "Shrub", "Wetland", "Unknown/Other"),
+                     colour =c("4CAF50", "1B5E20","E65100", "FF9800","6D4C41","A1887F","A5D6A7","9C27B0",
+                                "F06292","26C6DA","0288D1","607D8B"))
     colos <- tp$colour[which(levels(variable) == tp$habitat)]
     return(colos)
     }
@@ -16,9 +17,8 @@ ColourPicker <- function(variable){
     colos <- c(rep(NA, length(levels(variable))))
     
     tp <- data.frame(habitat = c("Pasture","Primary vegetation", "Production - Arable","Production - Crop plantations",
-                                 "Production - Wood plantation", "Secondary vegetation"),
-                     colour =c("696346", "FFD300", "68089D", "103B9D", "0041D8", "1B1B1B"))
-    # colour = c("red", "green", "blue", "grey", "purple", "black"))
+                                 "Production - Wood plantation", "Secondary vegetation", "Unknown"),
+                     colour =c("FFEB3B", "1B5E20", "E65100", "FF9800", "795548", "4CAF50", "607D8B"))
     colos <- tp$colour[which(levels(variable) == tp$habitat)]
     return(colos)
     
