1bb8c90ab26585b13982ea1bd0b25aaf9661c5cf
helenphillips <helen.r.phillips@googlemail.com>
2017-03-22 18:24:26 +0100

    Starting the task of drilobase data

--- CountrylevelAnalysis/0_GetData.R
+++ CountrylevelAnalysis/0_GetData.R
@@ -24,7 +24,7 @@ require(RJSONIO)
 ########################################################
 # 4. Load data and sort out data
 ########################################################
-
+## Species by country
 json_file <- "C://Users//hp39wasi//Google Drive//sWorm//CountryLevelAnalysis//dataDriloBASE.json"
 
 json_file <- fromJSON(json_file)
@@ -47,10 +47,51 @@ for(i in 1:length(json_file)){
 ## Remove first row
 dat <- dat[-1,]
 
+
+### Species information
+json_file <- "C://Users//hp39wasi//Google Drive//sWorm//CountryLevelAnalysis//speciesData.json"
+json_file <- fromJSON(json_file)
+
+species <- data.frame(name = NA,family = NA, genus = NA, geographicalOrigin = NA, distributionStatus = NA, ecologicalCategory = NA)
+
+for(i in 1:length(json_file)){
+  t <- json_file[[i]]
+  species[i,'name'] <- t['name']
+  species[i,'family'] <- t['family']
+  species[i,'genus'] <- t['genus']
+  species[i,'geographicalOrigin'] <- t['geographicalOrigin']
+  species[i,'distributionStatus'] <- t['distributionStatus']
+  species[i,'ecologicalCategory'] <- t['ecologicalCategory']
+  
+}
+
+species$name <- sub("_", " ", species$name)
 ########################################################
 # 5. Save the data
 ########################################################
 
 write.csv(dat, file = file.path(data_out, "CountrySpeciesList.csv"), row.names = FALSE)
+write.csv(species, file = file.path(data_out, "SpeciesList.csv"), row.names = FALSE)
 
 
+
+########################################################
+# 6.Amalgamate data
+########################################################
+c <- read.csv(file.path(data_out, "CountrySpeciesList.csv"))
+s <- read.csv(file.path(data_out, "SpeciesList.csv"))
+
+CountryAndSpecies <- merge(c, s, by.x = "species", by.y = "name", all.x = TRUE)
+
+
+CountryAndSpecies <- CountryAndSpecies[with(CountryAndSpecies, order(Country, species)), ]
+write.csv(CountryAndSpecies, file = file.path(data_out, "CountryAndSpeciesList.csv"), row.names = FALSE)
+
+
+
+
+
+### Just want the list of countries for Alberto
+
+countries <- data.frame(Country = unique(c$Country))
+write.csv(countries, file = file.path(data_out, "CountryList.csv"), row.names = FALSE)
