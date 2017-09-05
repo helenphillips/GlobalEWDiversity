892f103bbb851b8605343bd1b2116d54e0fd5bd0
helenphillips <helen.r.phillips@googlemail.com>
2017-09-05 22:27:35 +0200

    Updating code for latest template

--- 0_GetData.R
+++ 0_GetData.R
@@ -29,15 +29,15 @@ source("Functions/FormatData.R")
 x <- gs_ls() ## Authentication
 
 
+
 ########################################################
 # 5. Create dataframe for bibliographic and author info
 ########################################################
+template <- "DatasetTemplateoriginal"
+template_meta <- gs_title(template)
+meta <- as.data.frame(gs_read(template_meta, ws = "MetaData", col_names = FALSE))
 
-bib_names <- c("Article_Title", "Article_Year", "Article_FirstAuthorSurname",
-         "Article_Journal","Article_DOI","DataProvider_Title", "DataProvider_Surname",
-         "DataProvider_FirstName", "DataProvider_MiddleInitials", "DataProvider_Email",
-         "DataProvider_Institute", "DataProvider_Department", "Number_of_Studies",
-         "Total_Number_ofSites","Total_Number_ofSpecies", "Notes","BibKey")
+bib_names <- meta[,1]
 
 bib <- data.frame(matrix(ncol = length(bib_names), nrow = 0))
 colnames(bib) <- bib_names
@@ -50,8 +50,8 @@ all_files <- x$sheet_title[grep("^\\d*\\_", x$sheet_title, perl = TRUE)]
 
 cat(paste("\nFound", length(all_files), "datasheets"))
 
-all_bib <- list()
-names(all_bib) <- bib_names
+#all_bib <- list(length = length(bib_names))
+#names(all_bib) <- bib_names
 all_sites <- list()
 all_species <- list()
 
@@ -72,56 +72,62 @@ for(file in all_files){
   
   ## Adding article ID
   sites <- cbind(file, sites)
-  species <- cbind(file, species)
+  if(nrow(species) > 0){species <- cbind(file, species)}
   
   
   sites <- formatSites(sites)
-  species <- formatSpecies(species)
+  if(nrow(species) > 0){species <- formatSpecies(species)}
   
   ## Validation of site and species level data
-  already_checked <- c("4643_Nieminen2011", "230_Falco2015", "279_Pansu2015", "665_Raty2004", "000_Guernion2014")
-  if(!(file %in% already_checked)){
-    if(!(all(levels(sites$Study_site) %in% levels(species$Study_site)))) stop("Validation failed: Not all sites have species information")
-  }
-  rm(already_checked)
+  # already_checked <- c("4643_Nieminen2011", "230_Falco2015", "279_Pansu2015", "665_Raty2004", "000_Guernion2014")
+  # if(!(file %in% already_checked)){
+  #  if(!(all(levels(sites$Study_site) %in% levels(species$Study_site)))) stop("Validation failed: Not all sites have species information")
+  # }
+  # rm(already_checked)
+  
+  
   ## TODO: Check that dates make sense
   
   
-  #### Calculate site level species richness & Check the values there
-  juvs <- which(species$LifeStage == "Juvenile")
-  notSpecies <- which(is.na(species$SpeciesBinomial) && is.na(species$MorphospeciesID))
+  #### Calculate site level species richness from species list & Check the values there
+  if(nrow(species) > 0){
+    juvs <- which(species$LifeStage == "Juvenile")
+    notSpecies <- which(is.na(species$SpeciesBinomial) && is.na(species$MorphospeciesID))
   
-  if(length(c(juvs, notSpecies)) > 0){
-    spR <- as.data.frame(table(species$Study_site[-c(juvs, notSpecies)]))
-  } else {spR <- as.data.frame(table(species$Study_site))}
-  rm(juvs)
-  rm(notSpecies)
-  names(spR)[2] <- "NumberofSpecies"
-  sites <- merge(sites, spR, by.x = "Study_site", by.y = "Var1")
-  rm(spR)
+    if(length(c(juvs, notSpecies)) > 0){
+      spR <- as.data.frame(table(species$Study_site[-c(juvs, notSpecies)]))
+    } else {spR <- as.data.frame(table(species$Study_site))}
+    rm(juvs)
+    rm(notSpecies)
+    names(spR)[2] <- "NumberofSpecies"
+    sites <- merge(sites, spR, by.x = "Study_site", by.y = "Var1")
+    rm(spR)
   
   ## Calculate site level abundance
-  ta <- as.data.frame(tapply(species$Abundance, species$Study_site, sum))
-  names(ta) <- "Site_NumberofIndividuals"
-  ta$SS <- rownames(ta)
-  sites <- merge(sites, ta, by.x = "Study_site", by.y = "SS")
-  rm(ta)
+    ta <- as.data.frame(tapply(species$Abundance, species$Study_site, sum))
+    names(ta) <- "Site_NumberofIndividuals"
+    ta$SS <- rownames(ta)
+    sites <- merge(sites, ta, by.x = "Study_site", by.y = "SS")
+    rm(ta)
   
   ## Calculate site level biomass
   ## TODO
   
   ## Check if site level species richness values were given
-  check <- which(!(is.na(sites$SpeciesRichness)) && (sites$SpeciesRichnessUnit == "Number of species"))
-  if(length(check) > 0){
-    if(any(sites$SpeciesRichness[check] != sites$NumberofSpecies[check])){cat(paste("\n", file, ":Some of the site level species richness values do not add up"))}
+    check <- which(!(is.na(sites$SpeciesRichness)) && (sites$SpeciesRichnessUnit == "Number of species"))
+    if(length(check) > 0){
+      if(any(sites$SpeciesRichness[check] != sites$NumberofSpecies[check])){cat(paste("\n", file, ":Some of the site level species richness values do not add up"))}
+    }
+    rm(check)
   }
-  rm(check)
-
   ## Now to make a species level dataframe with all the variables in
-  site_species <- merge(species, sites, by.x = "Study_site", by.y = "Study_site", all.x = TRUE)
+  if(nrow(species) > 0){site_species <- merge(species, sites, by.x = "Study_site", by.y = "Study_site", all.x = TRUE)
+  all_species[[count]] <- site_species
+    }
+  
   
   all_sites[[count]] <- sites
-  all_species[[count]] <- site_species
+  
   
 }
 
