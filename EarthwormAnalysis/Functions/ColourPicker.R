660b2bb3ccc8170a688060530e457cc81feb767f
helenphillips <helen.r.phillips@googlemail.com>
2017-09-11 18:42:43 +0200

    Trying to sort code for models and plotting

--- EarthwormAnalysis/Functions/ColourPicker.R
+++ EarthwormAnalysis/Functions/ColourPicker.R
@@ -9,9 +9,7 @@ ColourPicker <- function(variable){
                                  "Paddy field", "Sparse vegetation", "Shrub", "Wetland", "Unknown/Other"),
                      colour =c("4CAF50", "1B5E20","E65100", "FF9800","6D4C41","A1887F","A5D6A7","26C6DA",
                                 "F06292","9C27B0","0288D1","607D8B"))
-    colos <- tp$colour[match(levels(variable), tp$habitat)]
-    
-    return(colos)
+
     }
   
   if(any(levels(variable) %in% c("Pasture", "Primary vegetation", "Production - Arable"))){
@@ -20,13 +18,21 @@ ColourPicker <- function(variable){
     tp <- data.frame(habitat = c("Pasture","Primary vegetation", "Production - Arable","Production - Crop plantations",
                                  "Production - Wood plantation", "Secondary vegetation", "Unknown"),
                      colour =c("FFEB3B", "1B5E20", "E65100", "FF9800", "795548", "4CAF50", "607D8B"))
-    colos <- tp$colour[match(levels(variable), tp$habitat)]
-    return(colos)
     
   }
   
+  if(any(levels(variable) %in% c("Annual crop", "Perennial crops","Integrated systems"))){
+    colos <- c(rep(NA, length(levels(variable))))
+    
+    tp <- data.frame(habitat = c("Primary vegetation","Secondary vegetation","Annual crop",            
+                            "Perennial crops","Integrated systems","Tree plantations",       
+                                "Pastures (grazed lands)","Urban", "Unknown"),
+                     colour = c("FFEB3B", "1B5E20", "E65100", "FF9800", "795548", "4CAF50", "607D8B","FFEB3B", "1B5E20"))
+    }
   
+  colos <- tp$colour[match(levels(variable), tp$habitat)]
   
+  return(colos)
 }
 
 
