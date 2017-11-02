97756877326ee487a9b6cbefdd2169e60f51fabd
helenphillips <helen.r.phillips@googlemail.com>
2017-11-02 18:30:55 +0100

    More working on sorting out species names including svaing to googlesheets

--- 1_SpeciesNames.R
+++ 1_SpeciesNames.R
@@ -51,30 +51,37 @@ dat <- read.csv(file.path(data_in, loadin))
 
 length(unique(dat$SpeciesBinomial)) ## 187
 
-sort(levels(dat$SpeciesBinomial))
+table(dat$Functional_Type) ## Only unknown for 310
 
-
+## Check that all are binomials
+which(
+  (sapply(gregexpr("\\W+", dat$SpeciesBinomial), length) + 1) 
+  < 2) ## This counts the number of words in teh string
 #################################################
 # 7. Create dataframe for new morphospecies
 #################################################
 
-spp <- data.frame(original = sort(levels(dat$SpeciesBinomial)))
+result <- dat %>%
+   group_by(SpeciesBinomial) %>%
+   summarise(strings = toString(unique(Functional_Type)))
 
-#################################################
-# 8. Tally of species names 
-#################################################
-sppN <- as.data.frame(table(dat$SpeciesBinomial))
+spp <- as.data.frame(result)
+spp$strings <- gsub("NA, ", "", spp$strings)
+spp$strings <- gsub(", NA", "", spp$strings)
+spp$strings <- gsub("Unknown, ", "", spp$strings)
+spp$strings <- gsub(", Unknown", "", spp$strings)
 
-spp <- (merge(spp, sppN, by.x = "original", by.y = "Var1"))
+names(spp)[2] <- "FunctionalGroup"
 
-# Take first hundred most predominant names
-popular <- spp$original[order(spp$Freq, decreasing = TRUE)]
-popular <- popular[1:30]
+## Remove line that isn't a species
+spp <- spp[-which(is.na(spp$SpeciesBinomial)),]
 
-s <- adist(spp$original, popular)
-i <- apply(s, 1, which.min) ## Gives the index
-spp$Proposed <- popular[i]
+## Remove Microdriles and Megadriles
+spp <- spp[-grep("microdrile", spp$SpeciesBinomial, ignore.case = TRUE),]
+spp <- spp[-grep("Megadrile", spp$SpeciesBinomial, ignore.case = TRUE)]
 
+## There's one that contains 'sub'
+unique(dat$file.x[grep("sub", dat$SpeciesBinomial, ignore.case = TRUE)])
 #################################################
 # 9. DriloBASE data
 #################################################
@@ -87,16 +94,52 @@ drilo <- as.data.frame(gs_read(drilobase, ws = "Sheet1"))
 # 10. Map our dataset
 ###############################################
 
-s2 <- adist(spp$original, drilo$name)
+s2 <- adist(spp$SpeciesBinomial, drilo$name)
 i2 <- apply(s2, 1, which.min) ## Gives the index
 spp$Drilobase <- drilo$name[i2]
 
-spp <- (merge(spp, drilo, by.x = "Drilobase", by.y = "name", all.x = TRUE))[,c(2, 3, 4, 1, 10)]
-names(spp)[5] <- "drilobase_fg"
+cols <-c("SpeciesBinomial", "FunctionalGroup", "Drilobase", "Author of species", "ecologicalCategory")
+spp <- (merge(spp, drilo, by.x = "Drilobase", by.y = "name", all.x = TRUE))
+spp <- spp [,names(spp) %in% cols]
+spp <- spp[c(2, 3, 1, 4, 5),]
+names(spp) <- c("original", "original_fg", "drilobase", "Authority of species", "drilobase_fg")
 
+###################################################
+# 111. Add blank columns for people to fill in
+###############################################
+
+
+spp$Revised <- NA
+spp$Revised_fg <- NA
+spp$Revised_Authority <- NA
+spp$sWormMember <- NA
 
 ###################################################
 # 11. Save the data
 ###############################################
 
-write.csv(spp, file.path(data_out, "UniqueSpecies.csv"), row.names = FALSE)
+write.csv(spp, file.path(data_out, paste("UniqueSpecies_", Sys.Date(), ".csv", sep ="")), row.names = FALSE)
+
+#Also need to save to google sheets, which is where everyone will edit
+output <- "UniqueSpecies+FunctionalGroups"
+output <- gs_title(output)
+output <- (gs_gs(output))
+
+t <- try(output <- output %>% 
+  gs_ws_new(ws_title = "UniqueSpecies+FunctionalGroups", input = spp,
+            trim = TRUE, verbose = FALSE), silent = TRUE)
+
+if(class(t) == "try-error"){ ## Googlesheet already exists, need to delete old one
+  output <- output %>% 
+    gs_ws_delete(ws = "UniqueSpecies+FunctionalGroups")
+  
+  ## And upload new one
+  output <- output %>% 
+    gs_ws_new(ws_title = "UniqueSpecies+FunctionalGroups", input = spp,
+              trim = TRUE, verbose = FALSE)
+  
+}
+
+output %>% 
+  gs_read(ws = 2)
+
