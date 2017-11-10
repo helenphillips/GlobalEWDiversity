d656280d76b66eb8377c85b1d430b3133301ab27
helenphillips <helen.r.phillips@googlemail.com>
2017-11-10 19:10:00 +0100

    Functions for modelling from lme4 wiki

---
+++ Functions/lme4_ModellingFunctions.R
@@ -0,0 +1,19 @@
+#### All functions from the lme4 wiki pages
+
+
+
+overdisp_fun <- function(model) {
+  ## number of variance parameters in 
+  ##   an n-by-n variance-covariance matrix
+  vpars <- function(m) {
+    nrow(m)*(nrow(m)+1)/2
+  }
+  model.df <- sum(sapply(VarCorr(model),vpars))+length(fixef(model))
+  rdf <- nrow(model.frame(model))-model.df
+  rp <- residuals(model,type="pearson")
+  Pearson.chisq <- sum(rp^2)
+  prat <- Pearson.chisq/rdf
+  pval <- pchisq(Pearson.chisq, df=rdf, lower.tail=FALSE)
+  c(chisq=Pearson.chisq,ratio=prat,rdf=rdf,p=pval)
+}
+
