928c830657f1a76ebca131b37b91df39edb7588e
helenphillips <helen.r.phillips@googlemail.com>
2017-11-28 13:19:45 +0100

    Successfully downloading soil grids data

--- EarthwormAnalysis/3.1_SoilGrids.R
+++ EarthwormAnalysis/3.1_SoilGrids.R
@@ -1,9 +1,25 @@
 library(XML)
 library(RCurl)
 
-sg.ftp <- "ftp://ftp.soilgrids.org/data/recent/"
+savedir <- "C:\\Users\\hp39wasi\\Dropbox\\sWorm\\SoilGrids\\1km"
+
+  
+  
+
+sg.ftp <- "ftp://ftp.soilgrids.org/data/aggregated/1km/"
 filenames = getURL(sg.ftp, ftp.use.epsv = TRUE, dirlistonly = TRUE)
 filenames = strsplit(filenames, "\r*\n")[[1]]
-> filenames[1:5]
-[1] "ACDWRB_M_ss_250m_ll.tif"     "ACDWRB_M_ss_250m_ll.tif.xml" "AWCh1_M_sl1_250m_ll.tif"
-[4] "AWCh1_M_sl1_250m_ll.tif.xml" "AWCh1_M_sl2_250m_ll.tif"
+filenames[1:5]
+
+
+dl <- filenames[grep("PHIHOX", filenames)]
+dl <- c(dl, filenames[grep("CLYPPT", filenames)])
+dl <- c(dl, filenames[grep("SLTPPT", filenames)])
+dl <- c(dl, filenames[grep("SNDPPT", filenames)])
+dl <- c(dl, filenames[grep("CECSOL", filenames)])
+dl <- c(dl, filenames[grep("ORCDRC", filenames)])
+dl <- c(dl, filenames[grep("TAXNWRB_1", filenames)])
+
+for(i in dl){
+  try(download.file(file.path(sg.ftp, i), file.path(savedir, i)))
+}
