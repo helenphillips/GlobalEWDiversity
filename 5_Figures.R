a85190d105a0cbdae9f31f3c3e14a6b6b6a7f955
helenphillips <helen.r.phillips@googlemail.com>
2017-12-05 10:39:32 +0100

    Changing script to work with newly saved models and data

--- 5_Figures.R
+++ 5_Figures.R
@@ -23,7 +23,7 @@ source("Functions/FormatData.R")
 # 2. Loading in variables
 #################################################
 
-data_in <-"3_Data"
+data_in <-"4_Data"
 
 files <- list.files(file.path(data_in))
 file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
@@ -31,10 +31,7 @@ file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by
 
 file_dates <- as.Date(file_dates)
 date <- max(file_dates, na.rm = TRUE)
-loadin <- files[grep(date, files)]
-
-rm(files)
-rm(date)
+# loadin <- files[grep(date, files)]
 
 if(!dir.exists("Figures")){
   dir.create("Figures")
@@ -45,14 +42,16 @@ figures <- "Figures"
 # 3. Load in data
 #################################################
 
-sites <- read.csv(file.path(data_in, loadin))
-rm(loadin)
+richness <- read.csv(file.path(data_in, paste("sitesRichness_",date,".csv", sep = "")))
+biomass <- read.csv(file.path(data_in, paste("sitesBiomass_",date,".csv", sep = "")))
+abundance <- read.csv(file.path(data_in, paste("sitesAbundance_",date,".csv", sep = "")))
 
-sites$scalePH <-scale(sites$ph_new)
-sites_habitat <- sites[sites$HabitatCover != "Unknown/Other",]
-sites_lu <- droplevels(sites[sites$LandUse != "Unknown",])
-sites <- SiteLevels(sites) 
-levels(sites)[levels(sites) == "Annual crop"] <- "Annual crops"
+richness <- droplevels(SiteLevels(richness))
+biomass <- droplevels(SiteLevels(biomass))
+abundance <- droplevels(SiteLevels(abundance))
+
+
+
 #################################################
 # 4. Load in models
 #################################################
@@ -60,45 +59,70 @@ levels(sites)[levels(sites) == "Annual crop"] <- "Annual crops"
 models <- "Models"
 
 
-load(file.path(models, "biomass_ESApH+CHELSA.rds"))
-load(file.path(models, "abundance_ESApH+CHELSA.rds.rds"))
-
-
-# levels(b2a@frame$LU_Mgmt)[which(levels(b2a@frame$LU_Mgmt) == "Annual crop")] <- "Annual crops"
-# levels(a1@frame$LU_Mgmt)[which(levels(a1@frame$LU_Mgmt) == "Annual crop")] <- "Annual crops"
-# levels(a2a@frame$LU_Mgmt)[which(levels(a2a@frame$LU_Mgmt) == "Annual crop")] <- "Annual crops"
-
-#################################################
-# BIOMASS
-#################################################
-biomass <- sites[complete.cases(sites$logBiomass),]
-biomass <- droplevels(biomass[biomass$ESA != "Unknown",])
-
-
-table(biomass$ESA)
-biomass_notinclude <- c("Tree open", "Sparse vegetation", "Cropland/Other vegetation mosaic", "Urban", "Paddy field", "Herbaceous with spare tree/shrub")
-
-biomass <- droplevels(biomass[!(biomass$ESA %in% biomass_notinclude),])
-biomass$scalePH <- as.vector(scale(biomass$ph_new))
-
-biomass$bio10_5_scaled <- scale(biomass$bio10_5)
-biomass$bio10_13_scaled <- scale(biomass$bio10_13)
-biomass$bio10_14_scaled <- scale(biomass$bio10_14)
-
+load(file.path(models, "richnessmodel_full.rds"))
+load(file.path(models, "biomassmodel_full.rds"))
+load(file.path(models, "abundancemodel_full.rds"))
 
 #################################################
 # 5. Pick colors for figures
 #################################################
-# habitCols <- ColourPicker(sites_habitat$HabitatCover)
-# luCols <- ColourPicker(sites_lu$LandUse)
-habitCols <- ColourPicker(biomass$ESA)
+
+biomassCols <- ColourPicker(biomass$ESA)
+richnessCols <- ColourPicker(richness$ESA)
+abundanceCols <- ColourPicker(abundance$ESA)
+
+#############################################
+# ABUNDANCE
+###############################################
+
+# pdf(file = file.path(figures, "Abundance_ph.pdf"), height = 4)
+jpeg(file = file.path(figures, "Abundance_ph.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
+
+plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
+           Effect1 = "scalePH", 
+           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
+           legend.position, ylabel = "log(Abundance)", xlabel = "pH", otherContEffectsFun = "median")
+dev.off()  
+
+jpeg(file = file.path(figures, "Abundance_bio10_14.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
+plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
+           Effect1 = "bio10_14_scaled", 
+           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
+           legend.position, ylabel = "log(Abundance)", xlabel = "Precip. of driest month", otherContEffectsFun = "median")
+dev.off()  
+
+jpeg(file = file.path(figures, "Abundance_bio10_13.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
+plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
+           Effect1 = "bio10_13_scaled", 
+           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
+           legend.position, ylabel = "log(Abundance)", xlabel = "Precip. of wettest month", otherContEffectsFun = "median")
+dev.off()  
+
+jpeg(file = file.path(figures, "Abundance_bio10_5.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
+plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
+           Effect1 = "bio10_5_scaled", 
+           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
+           legend.position, ylabel = "log(Abundance)", xlabel = "Max temp. of warmest month", otherContEffectsFun = "median")
+dev.off()  
+
+
+
+jpeg(file = file.path(figures, "Abundance_ESA.jpg"), quality = 100, res = 200, height = 1000, width = 1500, pointsize = 12)
+plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"),
+           Effect1 = "ESA", 
+           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = habitCols, 
+           legend.position, ylabel = "log(Abundance)", xlabel = "", otherContEffectsFun = "median")
+dev.off()
+
+
+
 #################################################
 # 6. Figures
 #################################################
 ### Interactions
 #pdf(file = file.path(figures, "Biomass_ph.pdf"), height = 4)
 jpeg(file = file.path(figures, "Biomass_ph.jpg"), quality = 100, res = 200, height = 1000, width = 2000)
-plotSingle(model= b4a, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
+plotSingle(model= biomass_model, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
            Effect1 = "scalePH", 
                 responseVar = "logBiomass", seMultiplier = 1, data = biomass, cols = "000000", 
                 legend.position, ylabel = "log(Wet Biomass)", xlabel = "pH", otherContEffectsFun = "median")
@@ -146,155 +170,6 @@ plotSingle(model= b4a, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "
            responseVar = "logBiomass", seMultiplier = 1, data = biomass, cols = "000000", 
            legend.position, ylabel = "log(Wet Biomass)", xlabel = "Max temp. of warmest month", otherContEffectsFun = "median")
 dev.off()
-
-#####################################################################
-## Showing intensity for one management
-######################################################################
-# 
-# nd <- createNewdata(model= b2a, modelFixedEffects = c("intensity", "LU_Mgmt", "scalePH"), data = biomass)
-# nd <- predictValues(model = b2a, newdata = nd, responseVar = "logBiomass", re.form = NA, seMultiplier = 1)
-# nd <- nd[nd$LU_Mgmt == "Pastures (grazed lands)",]  
-# ref <- 0 ## Getting mean value of pH
-# nd <- nd[which(abs(nd$scalePH-ref)==min(abs(nd$scalePH-ref))),]
-# 
-# ## Colours
-# pastureCols <- c("#cd8500","#a46a00","#7b4f00","#523500","#000000")
-# 
-# 
-# ## Plot
-# # pdf(file = file.path(figures, "Biomass_PastureIntensity.pdf"), height = 4)
-# jpeg(file = file.path(figures, "Biomass_PastureIntensity.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-# 
-# par(mar=c(4.5, 4, 1, 1))
-# plot(-1e+05, -1e+05, ylim = c(min(nd$lower,na.rm = TRUE), max(nd$upper, na.rm = TRUE)),
-#      xlim = c(0, (nrow(nd)-1)),  ylab = "log(Wet Biomass)", xlab = "Management Intensity",  xaxt='n', axes = FALSE)
-# Axis(side = 2 )
-# errbar(0:(nrow(nd)-1), nd$logBiomass, nd$upper, nd$lower,
-#        add = TRUE, col = pastureCols, errbar.col = pastureCols, cex = 1.5)
-# axis(side=1, at = 0:(nrow(nd)-1), labels = levels(nd$intensity))
-# dev.off()
-
-
-#################################################
-# ABUNDANCE
-#################################################
-abundance <- sites[complete.cases(sites$Site_Abundance),]
-abundance <- droplevels(abundance[abundance$ESA != "Unknown",])
-abundance$scalePH <- scale(abundance$ph_new)
-
-abundance_notinclude <- c("Needleleaf deciduous forest", "Sparse vegetation",
-                          "Cropland/Other vegetation mosaic", "Bare area (consolidated", "Paddy field",
-                          "Wetland/Herbaceous", "Herbaceous with spare tree/shrub")
-
-abundance <- droplevels(abundance[!(abundance$ESA %in% abundance_notinclude),])
-
-abundance$bio10_5_scaled <- scale(abundance$bio10_5)
-abundance$bio10_13_scaled <- scale(abundance$bio10_13)
-abundance$bio10_14_scaled <- scale(abundance$bio10_14)
-
-
-#################################################
-# 5. Pick colors for figures
-#################################################
-# habitCols <- ColourPicker(sites_habitat$HabitatCover)
-# luCols <- ColourPicker(sites_lu$LandUse)
-habitCols <- ColourPicker(abundance$ESA)
-#################################################
-# 6. Figures
-#################################################
-
-# pdf(file = file.path(figures, "Abundance_ph.pdf"), height = 4)
-jpeg(file = file.path(figures, "Abundance_ph.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-
-plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
-           Effect1 = "scalePH", 
-           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
-           legend.position, ylabel = "log(Abundance)", xlabel = "pH", otherContEffectsFun = "median")
-dev.off()  
-
-jpeg(file = file.path(figures, "Abundance_bio10_14.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
-           Effect1 = "bio10_14_scaled", 
-           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
-           legend.position, ylabel = "log(Abundance)", xlabel = "Precip. of driest month", otherContEffectsFun = "median")
-dev.off()  
-
-jpeg(file = file.path(figures, "Abundance_bio10_13.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
-           Effect1 = "bio10_13_scaled", 
-           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
-           legend.position, ylabel = "log(Abundance)", xlabel = "Precip. of wettest month", otherContEffectsFun = "median")
-dev.off()  
-
-jpeg(file = file.path(figures, "Abundance_bio10_5.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"), 
-           Effect1 = "bio10_5_scaled", 
-           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = "000000", 
-           legend.position, ylabel = "log(Abundance)", xlabel = "Max temp. of warmest month", otherContEffectsFun = "median")
-dev.off()  
-
-
-
-jpeg(file = file.path(figures, "Abundance_ESA.jpg"), quality = 100, res = 200, height = 1000, width = 1500, pointsize = 12)
-plotSingle(model= a4b, modelFixedEffs = c("scalePH", "ESA", "bio10_14_scaled", "bio10_13_scaled", "bio10_5_scaled"),
-           Effect1 = "ESA", 
-           responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = habitCols, 
-           legend.position, ylabel = "log(Abundance)", xlabel = "", otherContEffectsFun = "median")
-dev.off()
-
-
-# pdf(file = file.path(figures, "Abundance_LuMgmtxIntensity.pdf"), height = 4)
-# par(mfrow=c(1,2), mar = c(2, 2, 0, 0))
-# plotInteraction(model = a2a, Effect1 = "LU_Mgmt", Effect2 = "intensity", 
-#                 modelFixedEffs = c("intensity", "LU_Mgmt", "scalePH"),
-#                 responseVar = "logAbundance", seMultiplier = 1.96, 
-#                 data = abundance, cols = luMgmtCols, legend.position = "topleft", 
-#                 ylabel = "log(Abundance)", xlabel = "Management and Intensity")
-# cols <- paste("#", luMgmtCols, sep ="")
-# plot(x=(1:3), y=c(rep(2.9, 3)), xlim=c(1.9, 7), ylim = c(0,3), axes =FALSE, col = cols[1], type="l", lwd=2, ylab="", xlab="")
-# points(x=(1:3), y=c(rep(2.5, 3)), col = cols[2], type="l", lwd=2)
-# points(x=(1:3), y=c(rep(2.1, 3)), col = cols[3], type="l", lwd=2)
-# points(x=(1:3), y=c(rep(1.7, 3)), col = cols[4], type="l", lwd=2)
-# points(x=(1:3), y=c(rep(1.3, 3)), col = cols[5], type="l", lwd=2)
-# points(x=(1:3), y=c(rep(0.9, 3)), col = cols[6], type="l", lwd=2)
-# points(x=(1:3), y=c(rep(0.5, 3)), col = cols[7], type="l", lwd=2)
-# text(c(3.3, 3.3, 3.3), y=c(2.9,2.5,2.1,1.7,1.3,0.9,0.5), labels = c(levels(biomass$LU_Mgmt)[1:7]), adj=c(0,0.5))
-# 
-# dev.off()
-# 
-# #pdf(file = file.path(figures, "Abundance_LuMgmt.pdf"), height = 4)
-# jpeg(file = file.path(figures, "Abundance_LuMgmt.jpg"), quality = 100, res = 200, height = 1000, width = 1500)
-# 
-# plotSingle(model= a2a, modelFixedEffs = c("intensity", "LU_Mgmt", "scalePH"), Effect1 = "LU_Mgmt", 
-#            responseVar = "logAbundance", seMultiplier = 1, data = abundance, cols = luMgmtCols, 
-#            legend.position, ylabel = "log(Abundance)", xlabel = "", otherContEffectsFun = "median")
-# dev.off()
-
-
-#####################################################################
-## Showing intensity for one management
-######################################################################
-
-# nd <- createNewdata(model= a2a, modelFixedEffects = c("intensity", "LU_Mgmt", "scalePH"), data = abundance)
-# nd <- predictValues(model = a2a, newdata = nd, responseVar = "logAbundance", re.form = NA, seMultiplier = 1)
-# nd <- nd[nd$LU_Mgmt == "Pastures (grazed lands)",]  
-# ref <- 0 ## Getting mean value of pH
-# nd <- nd[which(abs(nd$scalePH-ref)==min(abs(nd$scalePH-ref))),]
-# 
-# ## Colours
-# pastureCols <- c("#cd8500","#a46a00","#7b4f00","#523500","#000000")
-# 
-# 
-# ## Plot
-# pdf(file = file.path(figures, "Abundance_PastureIntensity.pdf"), height = 4)
-# par(mar=c(4.5, 4, 1, 1))
-# plot(-1e+05, -1e+05, ylim = c(min(nd$lower,na.rm = TRUE), max(nd$upper, na.rm = TRUE)),
-#      xlim = c(0, (nrow(nd)-1)),  ylab = "log(Abundance)", xlab = "Management Intensity",  xaxt='n', axes = FALSE)
-# Axis(side = 2 )
-# errbar(0:(nrow(nd)-1), nd$logAbundance, nd$upper, nd$lower,
-#        add = TRUE, col = "black", errbar.col = "black", cex = 1.5)
-# axis(side=1, at = 0:(nrow(nd)-1), labels = levels(nd$intensity))
-# dev.off()
 
 
 #################################################3
