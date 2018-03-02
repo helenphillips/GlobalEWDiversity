2f12f3b881df8fbe821da3602cddd2aff2bef5c0
helenphillips <helen.r.phillips@googlemail.com>
2018-03-02 09:24:51 +0100

    Making script slightly more efficient

--- SpatialGaps/AccessibilityAndClimate.R
+++ SpatialGaps/AccessibilityAndClimate.R
@@ -18,36 +18,45 @@ rasterOptions(tmpdir = "/work/phillips", chunksize = 524288, maxmemory = 1342177
 ## Load Accessibility layer
 acc <- raster(file.path(data_dir, "accessibility_to_cities_2015_v1.tif"))
 print("Loading accessibility layer")
-## Load past climate layers
-# precipitation
-# p_precip <- raster(file.path(CLdir, "CHELSA_pr_1985-2010.tif"))
-# Temperature
-# p_temp <- raster(file.path(CLdir, "CHELSA_tas_1985-2010.tif"))
+
 ## Load future climate layers
 # precipitation
-f_precip <- raster(file.path(data_dir, "CHELSA_pr_1985-2050.tif"))
 print("loaded precipitation layer")
+f_precip <- raster(file.path(data_dir, "CHELSA_pr_1985-2050.tif"))
+
+## They are different extents
+print("cropping precipitation layer")
+f_precip <- crop(f_precip, acc, filename= file.path(output_dir, "CHELSA_pr_1985-2050_crop.tif"), overwrite=TRUE)
+
+
+## If temperature change is above 20 (becuse I haven't divided by 10)
+print("doing subset based on precipitation layer")
+acc[f_precip > -20 & f_precip < 20] <- NA
+
+print("removing precipitation layer")
+rm(f_precip)
+
+
 # Temperature
+print("loading temperature layer")
 f_temp <- raster(file.path(data_dir, "CHELSA_tas_1985-2050.tif"))
-print("loaded temperature layer")
 
 
 ## They are different extents
-f_temp <- crop(f_temp, acc, filename= file.path(output_dir, "CHELSA_tas_1985-2050_crop.tif"))
-f_precip <- crop(f_precip, acc, filename= file.path(output_dir, "CHELSA_pr_1985-2050_crop.tif"))
-print("cropped layers")
+print("cropping temperature layer")
+f_temp <- crop(f_temp, acc, filename= file.path(output_dir, "CHELSA_tas_1985-2050_crop.tif"), overwrite=TRUE)
 
-## If temperature change is above 20 (becuse I haven't divided by 10)
-print("doing First subset")
+
+print("doing subset based on temperature")
 acc[f_temp < 20] <- NA
 
-print("doing second subset")
-## and if rainfall is 
-acc[f_precip > -20 & f_precip < 20] <- NA
+# Removing temperature raster
+rm(f_temp)
+
 
 ## and if accessibility is very low
 #  accessibility is in minutes. This is 24 hours
-print("doing third subset")
+print("doing subset based on acccesibility")
 acc[acc > 1440] <- NA
 
 
