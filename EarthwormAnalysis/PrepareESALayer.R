d319b1286151a2b4356c307ded2a4b91e34dd298
helenphillips <helen.r.phillips@googlemail.com>
2018-04-07 15:36:49 +0200

    Sorting out ESA global layer for cluster

---
+++ EarthwormAnalysis/PrepareESALayer.R
@@ -0,0 +1,59 @@
+library(raster)
+
+
+args <- commandArgs(trailingOnly = TRUE)
+
+GLs_folder <- args[1] # GLs_dir
+savefolder <- args[2] # output_dir
+
+
+print(GLs_folder)
+print(savefolder)
+
+rasterOptions(tmpdir = "/work/phillips", chunksize = 524288, maxmemory = 134217728)
+
+
+
+print("Loading ESA raster")
+r <- raster(file.path(GLs_folder, "ESACCI-LC-L4-LCCS-Map-1000m-P5Y-2010-v1.6.1.tif"))
+# getValues(r)
+# plot(r)
+
+print("Cropping based on soil layer")
+ph <- raster(file.path(GLs_folder,"PHIHOX_RichnessCutScaled.tif"))
+r <- crop(r, ph, filename= file.path(savefolder, "ESA_cropped.tif"), overwrite=TRUE)
+
+print("Changing values")
+r[r == 11] <- 10 # Production herbs
+r[r == 40] <- 30
+print("2 done")
+r[r == 61] <- 60
+r[r == 62] <- 60
+print("4 done")
+r[r == 71] <- 70
+r[r == 72] <- 70
+print("6 done")
+r[r == 81] <- 80
+r[r == 82] <- 80
+print("8 done")
+r[r == 121] <- 120
+r[r == 122] <- 120
+print("10 done")
+r[r == 152] <- 150
+r[r == 153] <- 150
+print("12 done")
+r[r == 140] <- 20
+r[r == 160] <- 20
+print("14 done")
+r[r == 170] <- 20
+r[r == 180] <- 20
+print("16 done")
+r[r == 202] <- 200
+r[r == 220] <- 20
+
+
+
+print("Saving!")
+r <- writeRaster(r,  filename=file.path(savefolder, "ESA_newValuesCropped.tif"), format="GTiff", overwrite=TRUE)
+
+print("Done!!!")
