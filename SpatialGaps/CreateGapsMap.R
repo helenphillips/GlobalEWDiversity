2eae280d9da1632bc89472347116074e0dd0924b
helenphillips <helen.r.phillips@googlemail.com>
2018-04-10 10:09:50 +0200

    Updating points to most recent cut of database

--- SpatialGaps/CreateGapsMap.R
+++ SpatialGaps/CreateGapsMap.R
@@ -1,24 +1,32 @@
 library(raster)
 library(dismo)
 library(maps)
+setwd("C:/Users/hp39wasi/sWorm/SpatialGaps")
 
-sites <- read.csv("C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\4_Data\\sitesRichness_2017-12-04.csv")
+all_data <-"C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\3_Data"
 
-map <- raster("C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\6_Data\\spRFinalRaster.tif")
-mask <- raster("AccClimateMask.tif")
+files <- list.files(file.path(all_data))
+file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
+file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
+file_dates <- as.Date(file_dates)
+date <- max(file_dates, na.rm = TRUE)
+all_data <- read.csv(file.path(all_data, paste("sites_",date,".csv", sep = "")))
 
-plot(map)
+coord<-aggregate(cbind(all_studies$Longitude__Decimal_Degrees, all_studies$Latitude__decimal_degrees), list(all_studies$Study_Name), mean)
 
-
-
-coord<-aggregate(cbind(sites$Longitude__Decimal_Degrees, sites$Latitude__decimal_degrees), list(sites$Study_Name), mean)
-coord <- coord[complete.cases(coord),]
 coord$X<-coord$Group.1
 coord<-coord[2:4]
 names(coord)<-c("Long", "Lat", "X")
 
 dsSPDF<-SpatialPointsDataFrame(coord[,1:2], data.frame(coord[,1:3]))
 proj4string(dsSPDF)<-CRS("+proj=longlat")
+
+
+# map <- raster("C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\6_Data\\spRFinalRaster.tif")
+mask <- raster("AccClimateMask.tif")
+
+plot(map)
+
 
 
 cs <- circles(coord[, c(1,2)], d=100000, lonlat = TRUE)
@@ -104,8 +112,10 @@ gc()
 ############################################
 ## Change in climate versus sampled points
 
-pdf(file = "ClimateChange_gaps.pdf")
+#pdf(file = "ClimateChange_gaps.pdf")
+jpeg(file = "ClimateChange_gaps.jpg", width = 1500, height = 1500)
 map("world",border="gray87",fill=TRUE, col="gray87",mar=rep(0,4))
 plot(mask, add = TRUE, legend = FALSE)
-points(dsSPDF, col="black", bg="black", cex= 1, pch=19)
+points(dsSPDF, col="black", bg="black", cex= 1.9, pch=19)
 dev.off()
+
