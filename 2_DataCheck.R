b77e9b4da4bc74a830fa07dcf8a61b344f154b52
helenphillips <helen.r.phillips@googlemail.com>
2018-05-28 06:42:59 +0200

    Adding more variables in dataframe and checking soilgrids

--- 2_DataCheck.R
+++ 2_DataCheck.R
@@ -23,6 +23,9 @@ data_out <- "2_Data"
 
 data_in <-"1_Data"
 files <- list.files(file.path(data_in))
+files <- files[grep("sitesWithChelsaAndSoilAndOthers_", files)]
+
+
 file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
 file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
 
