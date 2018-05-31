3bc5f423ac2e46ec69ce8729b0cd2e24f6e64d5d
helenphillips <helen.r.phillips@googlemail.com>
2018-05-31 17:56:41 +0200

    Re-doing one map without the cool package

--- 5.5_MapsOfGLVariables.R
+++ 5.5_MapsOfGLVariables.R
@@ -15,6 +15,13 @@ library(rgdal)
 library(raster)
 library(rasterVis)
 
+
+
+if(!dir.exists("Prelim_Figures")){
+  dir.create("Prelim_Figures")
+}
+figures <- "Prelim_Figures"
+
 ########################################################
 # 2. Load In
 ########################################################
@@ -26,46 +33,78 @@ plot(esa)
 
 ## Soil
 soil <- "I:\\sDiv-PostDocs-Work\\Phillips\\sWorm\\SpatialAnalysis\\SoilGrids\\1km"
-pH <- raster(file.path(soil, "PHIHOX_weighted.tif"))
-plot(pH)
 
+pH <- raster(file.path(soil, "PHIHOX_weighted.tif"))
+jpeg(filename = file.path(figures, "pH_SoilGrids.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
 par(mar=c(5, 4, 1, 1))
 levelplot(pH, par.settings = RdBuTheme, margin=FALSE)
-
-
+dev.off()
 
 OrgC <- raster(file.path(soil, "ORCDRC_weighted.tif"))
-plot(OrgC)
-
+jpeg(filename = file.path(figures, "OrgC_SoilGrids.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(OrgC, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
 
 clay <- raster(file.path(soil, "CLYPPT_weighted.tif"))
-plot(clay)
+jpeg(filename = file.path(figures, "Clay_SoilGrids.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(clay, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
 
 silt <- raster(file.path(soil, "SLTPPT_weighted.tif")) 
-plot(silt)
+jpeg(filename = file.path(figures, "Silt_SoilGrids.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(silt, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
 
 CEC <- raster(file.path(soil, "CECSOL_weighted.tif"))
-plot(CEC)
+jpeg(filename = file.path(figures, "CEC_SoilGrids.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(CEC, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
 
 ## Climate
 chelsa <- "I:\\sDiv-PostDocs-Work\\Phillips\\sWorm\\SpatialAnalysis\\CHELSAData\\BioClim"
 
 AnnPrecip <- raster(file.path(chelsa, "CHELSA_bio10_1.tif"))
-plot(AnnPrecip)
+jpeg(filename = file.path(figures, "AnnPrecip_Chelsa.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(AnnPrecip, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
+
 
 
 PrecipSeason <- raster(file.path(chelsa, "CHELSA_bio10_15.tif"))
-plot(PrecipSeason)
+jpeg(filename = file.path(figures, "PrecipSeason_Chelsa.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(PrecipSeason, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
+
 
 
 snow <- raster("I:\\sWorm\\Database\\snow\\snow_2015_sum.tif")
-plot(snow)
+jpeg(filename = file.path(figures, "Snow.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(snow, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
+
 
 PET <- raster("I:\\sWorm\\Database\\PET_he_annual\\pet_he_yr_TIF.tif")
+jpeg(filename = file.path(figures, "PET.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
 plot(PET)
+dev.off()
+
 
 PETSD <- raster("I:\\sWorm\\Database\\PET_he_monthly\\pet_he_SD.tif")
-plot(PETSD)
+jpeg(filename = file.path(figures, "PETSD.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(PETSD, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
 
 AI <- raster("I:\\sWorm\\Database\\AI_annual\\ai_yr_TIF.tif")
-plot(AI)
+AI <- AI/10000 
+jpeg(filename = file.path(figures, "AI.jpg"), width = 1500, height = 1000, quality = 150, pointsize = 14)
+par(mar=c(5, 4, 1, 1))
+levelplot(AI, par.settings = RdBuTheme, margin=FALSE)
+dev.off()
