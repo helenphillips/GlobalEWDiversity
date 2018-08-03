b63f6e0538660f1062921ae481073372cf12b619
helenphillips <helen.r.phillips@googlemail.com>
2018-08-03 22:59:16 +0200

    Adding code to get columns for corvif functions

--- EarthwormAnalysis/Functions/CorvifVariablePicker.R
+++ EarthwormAnalysis/Functions/CorvifVariablePicker.R
@@ -1,5 +1,23 @@
+df_variables <- function(data){
+toMatch <- c("phFinal",
+"ClayFinal", "SiltFinal",
+"CECSOL", "OCFinal",
+
+"^bio10_1$", "^bio10_4$",
+"^bio10_7$", "^bio10_12$",
+"^bio10_15$",
+
+"^Aridity$", "^PETyr$", "^PET_SD$")
+
+matches <- unique (grep(paste(toMatch,collapse="|"), 
+                        names(data), value=FALSE))
+
+return(matches)
+
+}
+
 findVariables <- function(df, VIFThreshold = 3){
-  for(var in 1:ncol(df)){
+  for(v in 1:ncol(df)){
     t <- corvif(df)
     maxVIF <- which(t == max(t))
     if(t[maxVIF,] > VIFThreshold){
