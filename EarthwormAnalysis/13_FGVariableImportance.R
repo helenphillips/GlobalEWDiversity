cccf7aa9c302a93509afdaa196c86a7007996af7
helenphillips <helen.r.phillips@googlemail.com>
2018-08-10 16:41:42 +0200

    Doing all variable importance plots and adding in new function

--- EarthwormAnalysis/13_FGVariableImportance.R
+++ EarthwormAnalysis/13_FGVariableImportance.R
@@ -169,21 +169,8 @@ dat <- melt(a)
 dat$X1 <- factor(dat$X1, levels = c("Anecics", "Endogeics","Epigeics"))
 dat$X2 <- factor(dat$X2, levels = c("ESA","Soil","Precipitation", "Temperature","Water Retention"))
 
-brewer.pal(9,"Purples")
-
 jpeg(file = file.path(figures, "variableImportance_BiomassFGs.jpg"), quality = 100, res = 200, width = 2000, height = 1000)
 p <- VariableImportancePlot(dat, lowColour = "#BCBDDC", highColour = "#25004b", yLab = "Functional Group Model")
-  
-
-p <- ggplot(data =  dat, aes(x = X2, y = X1), ylab = "Test") +
-  geom_tile(aes(fill = value), colour = "white") +
-  # geom_text(aes(label = sprintf("%1.2f",value)), vjust = 1) +
-  # scale_fill_gradient(low = "white", high = "grey28") +
-  # scale_fill_viridis(option="viridis", discrete=TRUE) +
-  scale_fill_continuous(low = "#BCBDDC", high = "#25004b", guide = "legend", guide_legend(title = ""), 
-                        labels = c("Least Important", "","", "", "Most Important")) + 
-  theme_classic() +
-  labs(dat = "Importance", x = "Variable Groups", y = "Functional Group Model")
 p
 dev.off()
 
@@ -307,30 +294,20 @@ dat <- melt(a)
 dat$X1 <- factor(dat$X1, levels = c("Anecics", "Endogeics","Epigeics"))
 dat$X2 <- factor(dat$X2, levels = c("ESA","Soil","Precipitation", "Temperature","Water Retention"))
 
-brewer.pal(9,"Purples")
-
 jpeg(file = file.path(figures, "variableImportance_AbundanceFGs.jpg"), quality = 100, res = 200, width = 2000, height = 1000)
-p <- ggplot(data =  dat, aes(x = X2, y = X1), ylab = "Test") +
-  geom_tile(aes(fill = value), colour = "white") +
-  # geom_text(aes(label = sprintf("%1.2f",value)), vjust = 1) +
-  # scale_fill_gradient(low = "white", high = "grey28") +
-  # scale_fill_viridis(option="viridis", discrete=TRUE) +
-  scale_fill_continuous(low = "#BCBDDC", high = "#25004b", guide = "legend", guide_legend(title = ""), 
-                        labels = c("Least Important", "","", "", "Most Important")) + 
-  theme_classic() +
-  labs(dat = "Importance", x = "Variable Groups", y = "Functional Group Model")
+p <- VariableImportancePlot(dat, lowColour = "#BCBDDC", highColour = "#25004b", yLab = "Functional Group Model")
 p
 dev.off()
 
 ###############################################################
-##  ABUNDANCE - with split groups
+##  SPECIES RICHNESS - with split groups
 ###############################################################
 # epi
 ESA <- "ESA"
-epi_Temperature <- c("bio10_4_scaled", "ScalePET")
-epi_Precip <- c("bio10_15_scaled", "SnowMonths_cat", "scaleAridity")
-epi_Soil <- c("scalePH","scaleORCDRC","scaleCLYPPT", "scaleSLTPPT", "scaleCECSOL")
-epi_WaterRetention <- c("scaleCLYPPT", "scaleSLTPPT", "bio10_15_scaled", "ScalePET", "scaleAridity")
+epi_Temperature <- c("ScalePET", "ScalePETSD")
+epi_Precip <- c("bio10_12_scaled", "SnowMonths_cat")
+epi_Soil <- c("scalePH","scaleCLYPPT", "scaleSLTPPT", "scaleCECSOL")
+epi_WaterRetention <- c("scaleCLYPPT", "scaleSLTPPT", "bio10_12_scaled", "ScalePET", "ScalePETSD")
 
 groups <- list(
   ESA = ESA,
@@ -341,29 +318,28 @@ groups <- list(
 )
 
 
-epi_abundance <- abundance[grep("Epi", abundance$variable),]
-epi_abundance <- scaleVariables(epi_abundance)
-epi_abundance$logValue <- log(epi_abundance$value + 1)
+epi_richness <- richness[grep("Epi", richness$variable),]
+epi_richness <- scaleVariables(epi_richness)
 
-epi_abundance_mainEffects <- c("scalePH", "scaleCLYPPT", "scaleSLTPPT", "scaleORCDRC", "scaleCECSOL", "bio10_4_scaled", 
-                               "bio10_15_scaled", "SnowMonths_cat", "scaleAridity", "ScalePET", "ESA")
+epi_richness_mainEffects <- c("scalePH", "scaleCLYPPT", "scaleSLTPPT", "scaleCECSOL",  
+                               "bio10_12_scaled", "SnowMonths_cat", "ScalePET","ScalePETSD", "ESA")
 
 
-epi_abund_rf <- randomForest(y = epi_abundance$logValue, x = epi_abundance[,names(epi_abundance) %in% epi_abundance_mainEffects], 
+epi_richness_rf <- randomForest(y = epi_richness$value, x = epi_richness[,names(epi_richness) %in% epi_richness_mainEffects], 
                              ntree=501, importance=TRUE, proximity = TRUE)
-varImpPlot(epi_abund_rf, type=1)
-varImpPlot(epi_abund_rf, type=2)
+varImpPlot(epi_richness_rf, type=1)
+varImpPlot(epi_richness_rf, type=2)
 
-epi_abundance_import_split <- group.importance(epi_abund_rf, groups = groups) 
+epi_richness_import_split <- group.importance(epi_richness_rf, groups = groups) 
 
 # endo
-endo_Temperature <- c("bio10_4_scaled", "ScalePET")
-endo_Precip <- c("bio10_15_scaled", "SnowMonths_cat", "scaleAridity")
-endo_Soil <- c("scalePH","scaleORCDRC","scaleCLYPPT", "scaleSLTPPT", "scaleCECSOL")
-endo_WaterRetention <- c("scaleCLYPPT", "scaleSLTPPT", "scaleAridity", "bio10_15_scaled", "ScalePET")
+endo_Temperature <- c("bio10_4_scaled")
+endo_Precip <- c("bio10_15_scaled")
+endo_Soil <- c("scalePH", "scaleSLTPPT", "scaleCECSOL")
+endo_WaterRetention <- c("scaleSLTPPT", "bio10_15_scaled")
 
 groups <- list(
-  ESA = ESA,
+  # ESA = ESA,
   endo_Temperature = endo_Temperature,
   endo_Precip = endo_Precip,
   endo_Soil = endo_Soil,
@@ -371,30 +347,27 @@ groups <- list(
 )
 
 
-endo_abundance <- abundance[grep("Endo", abundance$variable),]
-endo_abundance <- scaleVariables(endo_abundance)
-endo_abundance$value[which(endo_abundance$value < 0)] <- 0
-endo_abundance$logValue <- log(endo_abundance$value + 1)
+endo_richness <- richness[grep("Endo", richness$variable),]
+endo_richness <- scaleVariables(endo_richness)
 
-endo_abundance_mainEffects <- c("scalePH", "scaleCECSOL", "scaleCLYPPT", "scaleSLTPPT", "scaleORCDRC", "bio10_4_scaled", "bio10_15_scaled",  
-                                "SnowMonths_cat", "ScalePET", "scaleAridity", "ESA")
+endo_richness_mainEffects <- c("scalePH", "scaleCECSOL", "scaleSLTPPT",  "bio10_4_scaled", "bio10_15_scaled")
 
 
-endo_abund_rf <- randomForest(y = endo_abundance$logValue, x = endo_abundance[,names(endo_abundance) %in% endo_abundance_mainEffects], 
+endo_richness_rf <- randomForest(y = endo_richness$value, x = endo_richness[,names(endo_richness) %in% endo_richness_mainEffects], 
                               ntree=501, importance=TRUE, proximity = TRUE)
-varImpPlot(endo_abund_rf, type=1)
-varImpPlot(endo_abund_rf, type=2)
+varImpPlot(endo_richness_rf, type=1)
+varImpPlot(endo_richness_rf, type=2)
 
-endo_abundance_import_split <- group.importance(endo_abund_rf, groups = groups) 
+endo_richness_import_split <- group.importance(endo_richness_rf, groups = groups) 
 
 # ane
-ane_Temperature <- c("bio10_4_scaled", "ScalePET")
-ane_Precip <- c("bio10_15_scaled", "SnowMonths_cat", "scaleAridity")
-ane_Soil <- c("scalePH","scaleCLYPPT", "scaleSLTPPT", "scaleCECSOL")
-ane_WaterRetention <- c("scaleCLYPPT", "scaleSLTPPT", "scaleAridity", "bio10_15_scaled", "ScalePET")
+ane_Temperature <- c("bio10_1_scaled")
+ane_Precip <- c("bio10_12_scaled", "SnowMonths_cat")
+ane_Soil <- c("scalePH","scaleSLTPPT")
+ane_WaterRetention <- c("scaleSLTPPT", "bio10_12_scaled")
 
 groups <- list(
-  ESA = ESA,
+  # ESA = ESA,
   ane_Temperature = ane_Temperature,
   ane_Precip = ane_Precip,
   ane_Soil = ane_Soil,
@@ -402,39 +375,38 @@ groups <- list(
 )
 
 
-ane_abundance <- abundance[grep("Ane", abundance$variable),]
-ane_abundance <- scaleVariables(ane_abundance)
-ane_abundance$value[which(ane_abundance$value < 0)] <- 0
-ane_abundance$logValue <- log(ane_abundance$value + 1)
+ane_richness <- richness[grep("Ane", richness$variable),]
+ane_richness <- scaleVariables(ane_richness)
 
-ane_abundance_mainEffects <- c("scalePH", "scaleCECSOL", "scaleCLYPPT", "scaleSLTPPT", "bio10_4_scaled", "bio10_15_scaled",  
-                               "SnowMonths_cat", "ScalePET", "scaleAridity", "ESA")
+ane_richness_mainEffects <- c("scalePH", "scaleSLTPPT", "bio10_1_scaled", "bio10_12_scaled",  
+                               "SnowMonths_cat")
 
 
-ane_abund_rf <- randomForest(y = ane_abundance$logValue, x = ane_abundance[,names(ane_abundance) %in% ane_abundance_mainEffects], 
+ane_richness_rf <- randomForest(y = ane_richness$value, x = ane_richness[,names(ane_richness) %in% ane_richness_mainEffects], 
                              ntree=501, importance=TRUE, proximity = TRUE)
-varImpPlot(ane_abund_rf, type=1)
-varImpPlot(ane_abund_rf, type=2)
+varImpPlot(ane_richness_rf, type=1)
+varImpPlot(ane_richness_rf, type=2)
 
-ane_abundance_import_split <- group.importance(ane_abund_rf, groups = groups) 
+ane_richness_import_split <- group.importance(ane_richness_rf, groups = groups) 
 
 ############
-# Abundance figure
+# Species Richness figure
 ############
 ## 5 is most important, 1 is least important
+# ESA, Temperature, Precip, Soil, WaterRetention
 
-epi_abundance_import_split
-epi_abundance_order <- c(1, 5, 2, 4, 3)
-endo_abundance_import_split
-endo_abundance_order <- c(5,4,1,3,2)
-ane_abundance_import_split
-ane_abundance_order <- c(1,5,2,4,3)
+epi_richness_import_split
+epi_richness_order <- c(1, 5, 2, 4, 3)
+endo_richness_import_split
+endo_richness_order <- c(NA, 5,2,4,3)
+ane_richness_import_split
+ane_richness_order <- c(NA,5,3,2,4)
 
 a <- matrix(rep(NA, length = 5*3), nrow = 3, ncol = 5)
 colnames(a) <- c("ESA", "Temperature", "Precipitation", "Soil", "Water Retention")
-a[1,] <- epi_abundance_order
-a[2,] <- endo_abundance_order
-a[3,] <- ane_abundance_order
+a[1,] <- epi_richness_order
+a[2,] <- endo_richness_order
+a[3,] <- ane_richness_order
 rownames(a) <- c("Epigeics", "Endogeics", "Anecics")
 
 dat <- melt(a)
@@ -442,18 +414,10 @@ dat <- melt(a)
 dat$X1 <- factor(dat$X1, levels = c("Anecics", "Endogeics","Epigeics"))
 dat$X2 <- factor(dat$X2, levels = c("ESA","Soil","Precipitation", "Temperature","Water Retention"))
 
-brewer.pal(9,"Purples")
 
-jpeg(file = file.path(figures, "variableImportance_AbundanceFGs.jpg"), quality = 100, res = 200, width = 2000, height = 1000)
-p <- ggplot(data =  dat, aes(x = X2, y = X1), ylab = "Test") +
-  geom_tile(aes(fill = value), colour = "white") +
-  # geom_text(aes(label = sprintf("%1.2f",value)), vjust = 1) +
-  # scale_fill_gradient(low = "white", high = "grey28") +
-  # scale_fill_viridis(option="viridis", discrete=TRUE) +
-  scale_fill_continuous(low = "#BCBDDC", high = "#25004b", guide = "legend", guide_legend(title = ""), 
-                        labels = c("Least Important", "","", "", "Most Important")) + 
-  theme_classic() +
-  labs(dat = "Importance", x = "Variable Groups", y = "Functional Group Model")
+jpeg(file = file.path(figures, "variableImportance_RichnessFGs.jpg"), quality = 100, res = 200, width = 2000, height = 1000)
+p <- VariableImportancePlot(dat, lowColour = "#BCBDDC", highColour = "#25004b", yLab = "Functional Group Model")
 p
 dev.off()
+
 
