fb73fa9814dc039ee4d724a7750a1a24f4aed0cf
helenphillips <helen.r.phillips@googlemail.com>
2018-08-21 17:13:53 +0200

    Submit script

---
+++ 16_submitScript_globaldatalayers-FG.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ 
+#$ -S /bin/bash
+#$ -N GlobalDataLayers
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+#$ -l h_rt=100:00:00
+#$ -l h_vmem=50G,highmem
+ 
+#$ -binding linear:1
+
+set -x
+
+export LANG=en_US.UTF8
+
+output_dir=/work/$USER/$JOB_NAME/$JOB_ID
+mkdir -p $output_dir
+data_dir=/data/idiv_sdiv/sworm/FG_Data
+date="2018-08-17"
+processed_dir=/data/idiv_sdiv/sworm/GlobalLayers
+module load R
+ 
+Rscript /home/phillips/16_PrepareGlobalLayers-FG.R $data_dir $output_dir $date $processed_dir
