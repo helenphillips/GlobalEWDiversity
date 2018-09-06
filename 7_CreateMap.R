1c7372771ae64aaf8f0a944c1169c1fb272d5aac
helenphillips <helen.r.phillips@googlemail.com>
2018-09-06 19:04:53 +0200

    Trying to get more results from maps

--- 7_CreateMap.R
+++ 7_CreateMap.R
@@ -227,7 +227,7 @@ dev.off()
 ############################################################
 
 
-# regions <- c("africa", "asia", "europe", "latin_america", "north_america", "west_asia")
+
 
 results <- "I:\\sDiv-PostDocs-Work\\Phillips\\sWorm\\SpatialAnalysis\\Results\\Abundance"
 
@@ -275,15 +275,17 @@ sdV <- mean(c(cellStats(africa, stat='sd', na.rm=TRUE, asSample=TRUE),
               cellStats(north_america, stat='sd', na.rm=TRUE, asSample=TRUE),
               cellStats(west_asia, stat='sd', na.rm=TRUE, asSample=TRUE)))
 
+
+
+###########################################
+
+
 colbrks <-  c(minV, seq(20, 100, length.out = 198), maxV)
 
 # seq(minV, maxV, length.out = 200)
 # actual <- c('#2F2C62', '#42399B', '#4A52A7', '#59AFEA', '#7BCEB8', '#A7DA64',
 #            '#EFF121', '#F5952D', '#E93131', '#D70131', '#D70131')
 # r.cols <- colorRampPalette(actual, space = "rgb")
-
-
-
 
 r.cols <- magma(199)
 
