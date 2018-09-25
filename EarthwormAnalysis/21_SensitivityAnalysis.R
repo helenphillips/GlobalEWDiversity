9f368e3d37a9e88287aa423fabef535c9af69a86
helenphillips <helen.r.phillips@googlemail.com>
2018-09-25 10:17:53 +0200

    Looking at global spread of badly predicted sites

--- EarthwormAnalysis/21_SensitivityAnalysis.R
+++ EarthwormAnalysis/21_SensitivityAnalysis.R
@@ -20,6 +20,32 @@ createSplits <- function(dat, kfold = 10){
 # 2. Load data
 #################################################
 
+data_in <-"3.5_Data"
+
+files <- list.files(file.path(data_in))
+file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
+file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
+
+file_dates <- as.Date(file_dates)
+date <- max(file_dates, na.rm = TRUE)
+loadin <- files[grep(date, files)]
+
+rm(files)
+rm(date)
+
+sites <- read.csv(file.path(data_in, loadin))
+
+data_in <-"4_Data"
+files <- list.files(file.path(data_in))
+files <- files[grep("sitesBiomass_", files)]
+file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
+file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
+
+file_dates <- as.Date(file_dates)
+date <- max(file_dates, na.rm = TRUE)
+loadin <- files[grep(date, files)]
+
+biomass <- read.csv(file.path(data_in, loadin))
 
 
 #################################################
@@ -54,6 +80,30 @@ spRData$Predicted <- exp(predict(richness_model, spRData, re.form = NA))
 opaqueBlack <- rgb(red = 0, green = 0, blue = 0, alpha = 0.3,maxColorValue = 1)
 plot(spRData$Predicted ~ jitter(spRData$SpeciesRichness), ylim = c(0, 15), pch = 19, col = opaqueBlack)
 abline(0, 1) 
+
+## Which have "bad" predictions
+
+badPreds <- droplevels(spRData[(spRData$SpeciesRichness) > 6,])
+levels(badPreds$file)
+
+dat <- droplevels(sites[sites$file %in% badPreds$file,])
+levels(dat$Country)
+
+summary(spRData)
+summary(badPreds)
+
+library(maps)
+library(maptools)
+coord<-aggregate(cbind(dat$Longitude__Decimal_Degrees, dat$Latitude__decimal_degrees), list(dat$Study_Name), mean)
+
+coord$X<-coord$Group.1
+coord<-coord[2:4]
+names(coord)<-c("Long", "Lat", "X")
+dsSPDF<-SpatialPointsDataFrame(coord[,1:2], data.frame(coord[,1:3]))
+proj4string(dsSPDF)<-CRS("+proj=longlat")
+mar=c(0,0,0,0)
+map("world",border="gray87",fill=TRUE, col="gray87",mar=rep(0,4))
+points(dsSPDF, col="black", bg="black", cex= 1, pch=19)
 
 
 #################################################
@@ -101,3 +151,13 @@ for(k in 1:k_fold){
 df <- do.call("rbind", predictedData)
 plot(df$predicted ~ df$observed)
 abline(0, 1) 
+
+
+### Biomass
+dat <- biomass_model@frame
+dat$resids <- residuals(biomass_model)
+dat$predicted <- (predict(biomass_model, dat, re.form = NA))
+plot(dat$predicted ~ dat$logBiomass)
+abline(0, 1) 
+
+
