1649d169c729873e0ad5e07f5afd390416a2daa6
helenphillips <helen.r.phillips@googlemail.com>
2018-10-12 14:41:20 +0200

    Finishing all analysis and creating plot

--- EarthwormAnalysis/21_SensitivityAnalysis.R
+++ EarthwormAnalysis/21_SensitivityAnalysis.R
@@ -16,6 +16,9 @@ createSplits <- function(dat, kfold = 10){
   
   return(splits)
 }
+
+
+figures <- "Figures"
 #################################################
 # 2. Load data
 #################################################
@@ -66,6 +69,20 @@ date <- max(file_dates, na.rm = TRUE)
 loadin <- files[grep(date, files)]
 abundance <- read.csv(file.path(data_in, loadin))
 
+## FG Richness
+data_in <- "15_Data"
+
+files <- list.files(file.path(data_in))
+files <- files[grep("sites\\+FGRichness_", files)]
+file_dates <- sapply(strsplit(files, "_"), "[", 2) ## Split the string by date, which produces a list, then take second element of each list i.e. the date
+file_dates <- sapply(strsplit(file_dates, "\\."), "[", 1) ## Split the string by date, which produces a list, then take first element of each list i.e. the date
+file_dates <- as.Date(file_dates)
+date <- max(file_dates, na.rm = TRUE)
+loadin <- files[grep(date, files)]
+fgrichness <- read.csv(file.path(data_in, loadin))
+
+
+
 #################################################
 # 3. Create directories
 #################################################
@@ -86,6 +103,13 @@ models <- "Models"
 load(file.path(models, "richnessmodel.rds"))
 load(file.path(models, "biomassmodel_full.rds"))
 load(file.path(models, "abundancemodel_full.rds"))
+load(file.path(models, "fgrichnessmodel.rds"))
+
+
+
+k_fold <- 10
+
+
 
 #################################################
 # 5. Species Richness
@@ -96,7 +120,7 @@ richnessData <- richness_model@frame
 ########
 # K-Fold Cross validation
 ########
-k_fold <- 10
+
 splits <- createSplits(richnessData, kfold = k_fold)
 
 predictedData <- list()
@@ -106,17 +130,8 @@ for(k in 1:k_fold){
   testData <- richnessData[rows,]
   bankData <- richnessData[-rows,]
   
-  mod <- glmer(SpeciesRichness ~ scalePH + scaleCLYPPT + scaleSLTPPT + scaleCECSOL +  
-                scaleORCDRC + bio10_7_scaled + bio10_15_scaled + SnowMonths_cat +  
-                scaleAridity + ScalePET + scalePH:scaleCECSOL + scalePH:scaleORCDRC +  
-                scaleCLYPPT:scaleCECSOL + scaleSLTPPT:scaleORCDRC + scaleCECSOL:scaleORCDRC +  
-                bio10_7_scaled:bio10_15_scaled + bio10_7_scaled:SnowMonths_cat +  
-                bio10_7_scaled:scaleAridity + bio10_15_scaled:SnowMonths_cat +  
-                bio10_15_scaled:scaleAridity + bio10_15_scaled:ScalePET +  
-                SnowMonths_cat:scaleAridity + scaleCLYPPT:bio10_15_scaled +  
-                scaleSLTPPT:ScalePET + scaleSLTPPT:scaleAridity + ESA + scaleElevation + 
-                (1 | file/Study_Name), data = bankData, family = poisson,
-              control = glmerControl(optCtrl = list(maxfun = 2e5), optimizer ="bobyqa"))
+  mod <-  glmer(formula = richness_model@call$formula, data = bankData, family = "poisson",
+                control = glmerControl(optimizer = "bobyqa",optCtrl=list(maxfun=2e5)))
   
   testData$Predicted <- (predict(mod, testData,  re.form = NULL, allow.new.levels = TRUE))
   
@@ -128,6 +143,7 @@ df <- do.call("rbind", predictedData)
 plot(df$predicted ~ df$observed)
 abline(0, 1) 
 
+richness <- df
 
 #################################################
 # 5. Abundance
@@ -139,7 +155,7 @@ abundanceData <- abundance_model@frame
 ########
 # K-Fold Cross validation
 ########
-  k_fold <- 10
+
 splits <- createSplits(abundanceData, kfold = k_fold)
 
 predictedData <- list()
@@ -149,17 +165,8 @@ for(k in 1:k_fold){
   testData <- abundanceData[rows,]
   bankData <- abundanceData[-rows,]
   
-  mod <- lmer(logAbundance ~ scalePH + scaleCLYPPT + scaleSLTPPT + scaleCECSOL +  
-                scaleORCDRC + bio10_7_scaled + bio10_15_scaled + SnowMonths_cat +  
-                scaleAridity + ScalePET + scalePH:scaleSLTPPT + scalePH:scaleCECSOL +  
-                scalePH:scaleORCDRC + scaleCLYPPT:scaleCECSOL + scaleCLYPPT:scaleORCDRC +  
-                scaleCECSOL:scaleORCDRC + bio10_7_scaled:bio10_15_scaled +  
-                bio10_7_scaled:SnowMonths_cat + bio10_7_scaled:scaleAridity +  
-                bio10_15_scaled:SnowMonths_cat + bio10_15_scaled:ScalePET +  
-                SnowMonths_cat:ScalePET + scaleAridity:ScalePET + scaleCLYPPT:bio10_15_scaled +  
-                scaleCLYPPT:ScalePET + scaleSLTPPT:ScalePET + ESA + ScaleElevation +  
-                (1 | file/Study_Name), data = bankData, 
-              control = lmerControl(optCtrl = list(maxfun = 2e5), optimizer ="bobyqa"))
+  mod <-  lmer(formula = abundance_model@call$formula, data = bankData, 
+               control = lmerControl(optimizer = "bobyqa",optCtrl=list(maxfun=2e5)))
   
   testData$Predicted <- (predict(mod, testData,  re.form = NULL, allow.new.levels = TRUE))
   
@@ -168,9 +175,13 @@ for(k in 1:k_fold){
 }
 
 df <- do.call("rbind", predictedData)
-plot(df$predicted ~ df$observed)
-abline(0, 1) 
 
+# jpeg(file = file.path(figures, "Abundance_Crossvalidation.jpg"), quality = 100, res = 200, width = 2000, height = 1000)
+plot(df$predicted ~ df$observed, ylab = "Predicted log-Abundance", xlab = "Observed log-Abundance", pch = 19, cex = 0.5)
+abline(0, 1) 
+# dev.off()
+
+abundance <- df
 
 #################################
 ## BIOMASS
@@ -181,7 +192,6 @@ biomassData <- biomass_model@frame
 # K-Fold Cross validation
 ########
 
-k_fold <- 10
 splits <- createSplits(biomassData, kfold = k_fold)
 
 predictedData <- list()
@@ -191,19 +201,10 @@ for(k in 1:k_fold){
   testData <- biomassData[rows,]
   bankData <- biomassData[-rows,]
   
-  mod <- lmer(logBiomass ~ scalePH + scaleCLYPPT + scaleSLTPPT + scaleORCDRC +  
-                scaleCECSOL + bio10_7_scaled + bio10_12_scaled + bio10_15_scaled +  
-                ScalePET + SnowMonths_cat + scalePH:scaleCLYPPT + scalePH:scaleSLTPPT +  
-                scalePH:scaleORCDRC + scalePH:scaleCECSOL + scaleCLYPPT:scaleCECSOL +  
-                scaleORCDRC:scaleCECSOL + bio10_7_scaled:bio10_12_scaled +  
-                bio10_12_scaled:bio10_15_scaled + bio10_12_scaled:ScalePET +  
-                bio10_12_scaled:SnowMonths_cat + bio10_15_scaled:ScalePET +  
-                bio10_15_scaled:SnowMonths_cat + ScalePET:SnowMonths_cat +  
-                scaleSLTPPT:bio10_12_scaled + scaleCLYPPT:bio10_15_scaled +  
-                ESA +
-                (1 | file/Study_Name), data = bankData, 
-              control = lmerControl(optCtrl = list(maxfun = 2e5), optimizer ="bobyqa"))
+  mod <-  lmer(formula = biomass_model@call$formula, data = bankData, 
+               control = lmerControl(optimizer = "bobyqa",optCtrl=list(maxfun=2e5)))
   
+           
   testData$Predicted <- (predict(mod, testData, re.form = NULL, allow.new.levels = TRUE))
   
   predictedData[[k]] <- data.frame(observed = testData$logBiomass, predicted = testData$Predicted)
@@ -214,4 +215,59 @@ df <- do.call("rbind", predictedData)
 plot(df$predicted ~ df$observed)
 abline(0, 1) 
 
+biomass <- df
+#################################
+## FG Richness
+##################################
+fgRichnessData <- fgrichness_model@frame
 
+########
+# K-Fold Cross validation
+########
+splits <- createSplits(fgRichnessData, kfold = k_fold)
+
+predictedData <- list()
+for(k in 1:k_fold){
+  
+  rows <- as.vector(unlist(splits[k]))
+  testData <- fgRichnessData[rows,]
+  bankData <- fgRichnessData[-rows,]
+  
+  mod <-  glmer(formula = fgrichness_model@call$formula, data = bankData, family = "poisson",
+                control = glmerControl(optimizer = "bobyqa",optCtrl=list(maxfun=2e5)))
+  
+  testData$Predicted <- (predict(mod, testData, re.form = NULL, allow.new.levels = TRUE))
+  
+  predictedData[[k]] <- data.frame(observed = testData$FGRichness, predicted = testData$Predicted)
+  
+}
+
+df <- do.call("rbind", predictedData)
+plot(df$predicted ~ df$observed)
+abline(0, 1) 
+
+fgrichness <- df
+
+####################################
+## PLOT
+####################################
+
+jpeg(file = file.path(figures, "AllModels_Crossvalidation.jpg"), quality = 100, res = 200, width = 2000, height = 1500)
+
+par(mar = c(2.5, 2.5, 1, 1))
+par(mfrow = c(2, 2))
+plot(exp(richness$predicted) ~ jitter(richness$observed), ylab = "", xlab = "", pch = 19, cex = 0.5)
+abline(0, 1) 
+text(x = -0.5, y = 12, labels = "Species Richness", pos = 4)
+plot(abundance$predicted ~ abundance$observed, ylab = "", xlab = "", pch = 19, cex = 0.5, ylim = c(0, 8))
+abline(0, 1) 
+text(x = -0.2, y = 7.4, labels = "(log)Abundance", pos = 4)
+plot(biomass$predicted ~ biomass$observed, ylab = "", xlab = "", pch = 19, cex = 0.5, ylim = c(0, 8))
+abline(0, 1) 
+text(x = -0.2, y = 7, labels = "(log)Biomass", pos = 4)
+
+plot(exp(fgrichness$predicted) ~ jitter(fgrichness$observed), ylab = "", xlab = "", pch = 19, cex = 0.5)
+abline(0, 1) 
+text(x = -0.3, y = 4, labels = "Functional Richness", pos = 4)
+
+dev.off()
