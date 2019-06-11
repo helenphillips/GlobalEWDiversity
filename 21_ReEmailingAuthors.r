ad84745ebda0b0f544d99d2cb137003bc1025eff
helenphillips <helen.r.phillips@googlemail.com>
2019-06-11 18:12:37 +0200

    Final email about authorship

--- 21_ReEmailingAuthors.r
+++ 21_ReEmailingAuthors.r
@@ -1,13 +1,44 @@
+############################################
+# Setting up working directory
+########################################### 
+
+
 setwd("C:/restore2/hp39wasi/sWorm/EarthwormAnalysis")
+
+############################################
+#Loading data
+########################################### 
+
+# I sent 6 different campaigns
+# So a list for each
 
 sent <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take1_sent_Mar_26_2019.csv")
 sent2 <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take2_sent_Mar_26_2019.csv")
+sent3 <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take3_sent_Jun_8_2019.csv")
+sent4 <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take4_sent_Jun_8_2019.csv")
+sent5 <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take5_sent_Jun_8_2019.csv")
+sent6 <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\members_DataProvider_Take6_sent_Jun_8_2019.csv")
+
+
+
+
+
+
+
+############################################
+# Formatting data
+########################################### 
 
 sent$source <- NULL
+sent3$source <- NULL
+sent4$source <- NULL
+sent5$source <- NULL
+sent6$source <- NULL
 
-sent <- rbind(sent, sent2)
+sent <- rbind(sent, sent2, sent3, sent4, sent5, sent6)
 
-form <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\Co-Authors_Responses_2019-03-26.csv")
+# form <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\Co-Authors_Responses_2019-03-26.csv")
+form <- read.csv("C:\\Google Drive\\sWorm\\sWorm_WS\\EarthwormAnalysis\\Manuscripts\\SpatialAnalysis\\DataProviders\\Co-Authors on the first analysis paper (Responses)_2019-03-31.csv")
 
 form_emails <- form$Email.address
 form_surnames <- form$Please.enter.your.surname..family.name.
@@ -31,8 +62,17 @@ tosend[which(tosend$Last.Name %in% form_surnames),]
 tosend <- tosend[-(which(tosend$Last.Name %in% form_surnames)),]
 
 
+
+## There are now duplicates
+
+duplicated(tosend$Email.Address) # 56 falses
+tosend <- tosend[!(duplicated(tosend$Email.Address)),]
+
+
+
+
 ## To this file, I will do a manual check (becuase of formatting of anmes)
 ## and append on previously bounced emails
 ## Then resend to the list
 
-write.csv(tosend, file = file.path("Authorship", "toResend_2019-03-26.csv"), row.names = FALSE)
+write.csv(tosend, file = file.path("Authorship", "toResend_2019-06-08.csv"), row.names = FALSE)
