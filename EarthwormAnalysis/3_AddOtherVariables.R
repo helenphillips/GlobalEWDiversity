39fe8adaa33635b1ea18f00593007a3cdc9c0331
helenphillips <helen.r.phillips@googlemail.com>
2019-06-17 16:48:03 +0200

    Adding in biome and world countries

--- EarthwormAnalysis/3_AddOtherVariables.R
+++ EarthwormAnalysis/3_AddOtherVariables.R
@@ -3,7 +3,7 @@
 ########################################################
 
 if(Sys.info()["nodename"] == "IDIVNB193"){
-  setwd("C:\\Users\\hp39wasi\\sWorm\\EarthwormAnalysis\\")
+  setwd("C:\\restore2\\hp39wasi\\sWorm\\EarthwormAnalysis\\")
 }
 
 #################################################
@@ -56,6 +56,7 @@ sites <- read.csv(file.path(data_in, loadinsites))
 ele <- "I:\\sWorm\\Elevation"
 tif <- raster(file.path(ele, "elevation.tif"))
 sites$elevation <- extract(tif, data.frame(sites$Longitude__Decimal_Degrees, sites$Latitude__decimal_degrees))
+
 ############## SNOW
 
 snow <- "I:\\sWorm\\Database\\snow"
@@ -79,6 +80,55 @@ petSD <- "I:\\sWorm\\Database\\PET_he_monthly"
 tif <- raster(file.path(petSD, "pet_he_SD.tif"))
 sites$PET_SD <- extract(tif, data.frame(sites$Longitude__Decimal_Degrees, sites$Latitude__decimal_degrees))
 sites$ScalePETSD <- scale(sites$PET_SD)
+rm(tif)
+
+############# BIomes
+library(sp)
+
+shape <- readOGR(dsn = "I:\\sDiv-PostDocs-Work\\Phillips\\sWorm\\SpatialAnalysis\\ecoregions\\official_teow\\official", layer = "wwf_terr_ecos")
+
+test <- sites[!(is.na(sites$Latitude__decimal_degrees)),]
+
+pts <- SpatialPoints(test[,c('Longitude__Decimal_Degrees','Latitude__decimal_degrees')], 
+                     proj4string = CRS(proj4string(shape)))
+
+test2 <- pts %over% shape
+
+biomes <- data.frame(ID = test$ID, biome = test2$BIOME)
+
+biomes$biome <- as.character(biomes$biome)
+biomes$biome[biomes$biome ==  1] <- "Tropical & Subtropical Moist Broadleaf Forests"
+biomes$biome[biomes$biome ==  2] <- "Tropical & Subtropical Dry Broadleaf Forests"
+biomes$biome[biomes$biome ==  3] <- "Tropical & Subtropical Coniferous Forests"
+biomes$biome[biomes$biome ==  4] <- "Temperate Broadleaf & Mixed Forests"
+biomes$biome[biomes$biome ==  5] <- "Temperate Conifer Forests"
+biomes$biome[biomes$biome ==  6] <- "Boreal Forests/Taig"
+biomes$biome[biomes$biome ==  7] <- "Tropical & Subtropical Grasslands, Savannas & Shrublands"
+biomes$biome[biomes$biome ==  8] <- "Temperate Grasslands, Savannas & Shrublands"
+biomes$biome[biomes$biome ==  9] <- "Flooded Grasslands & Savannas"
+biomes$biome[biomes$biome ==  10] <- "Montane Grasslands & Shrublands"
+biomes$biome[biomes$biome ==  11] <- "Tundra"
+biomes$biome[biomes$biome ==  12] <- "Mediterranean Forests, Woodlands & Scrub"
+biomes$biome[biomes$biome ==  13] <- "Deserts & Xeric Shrublands"
+biomes$biome[biomes$biome ==  14] <- "Mangroves"
+
+
+sites <- merge(sites, biomes, by = 'ID', all.x = TRUE)
+
+############## Country name
+
+shape <- readOGR(dsn = "I:\sDiv-PostDocs-Work\Phillips\sWorm\SpatialAnalysis\world\TM_WORLD_BORDERS_SIMPL-0.3", layer = "TM_WORLD_BORDERS_SIMPL-0.3")
+
+test <- sites[!(is.na(sites$Latitude__decimal_degrees)),]
+
+pts <- SpatialPoints(test[,c('Longitude__Decimal_Degrees','Latitude__decimal_degrees')], 
+                     proj4string = CRS(proj4string(shape)))
+
+test2 <- pts %over% shape
+biomes <- data.frame(ID = test$ID, biome = test2$BIOME)
+
+
+
 #################################################
 # 5. Save data
 #################################################
