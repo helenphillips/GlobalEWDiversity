bcd6ad84d27a77276e2aa60c680b0dffdb8552d5
helenphillips <helen.r.phillips@googlemail.com>
2019-06-24 19:23:59 +0200

    Rerunningfor new data

--- 14.1_submitScript_SoilversusSoilGrids_richness.sh
+++ 14.1_submitScript_SoilversusSoilGrids_richness.sh
@@ -18,7 +18,7 @@ export LANG=en_US.UTF8
 output_dir=/work/$USER/$JOB_NAME/$JOB_ID
 mkdir -p $output_dir
 data_in=/data/idiv_sdiv/sworm
-date="2018-11-08"
+date="2019-06-24"
 functions=/work/$USER/Functions
 
 
