c167a31639b0158eff6bb433bd3243c5677d70eb
helenphillips <helen.r.phillips@googlemail.com>
2019-07-05 11:02:57 +0200

    There must be a way to do this without aving so many files

---
+++ EarthwormAnalysis/PreparingGlobalLayers/submitScript_globaldatalayers_abundance_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ 
+#$ -S /bin/bash
+#$ -N GlobalDataLayers_abundance_1
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+#$ -l h_rt=100:00:00
+#$ -l h_vmem=50G,highmem
+ 
+#$ -binding linear:1
+
+set -x
+
+export LANG=en_US.UTF8
+
+output_dir=/work/$USER/$JOB_NAME/$JOB_ID
+mkdir -p $output_dir
+data_dir=/data/idiv_sdiv/sworm/GlobalLayers
+date="2019-06-18"
+site_dir=/data/idiv_sdiv/sworm
+module load R
+ 
+Rscript /home/phillips/PrepareGlobalLayers_abundance_1.R $data_dir $output_dir $date $site_dir
