ba7bee7015addb27d0a4987e0d3518909d863b41
Helen Phillips <helen.r.phillips@googlemail.com>
2019-07-11 18:57:23 +0200

    Format for paper

--- EarthwormAnalysis/11_CreateMap.R
+++ EarthwormAnalysis/11_CreateMap.R
@@ -20,6 +20,15 @@ library(maptools)
 library(sp)
 library(viridis)
 
+#######################################
+# variables
+######################################
+
+wide_cm <- 12
+wide_inch <- 4.75
+point_size <- 7
+plotlabcex <- 0.5
+resdpi <- 300
 ############################################################
 ## 'Global'
 ############################################################
@@ -29,8 +38,6 @@ bkg <- raster("D:\\sWorm\\ProcessedGL\\Datasets\\bio10_1.tif")
 ## The percentage that map is cut off at top and bottom
 
 
-plotlabcex <- 0.5
-resdpi <- 300
 ############################################################
 ## Species Richness
 ############################################################
@@ -54,13 +61,13 @@ north_america <- raster(file.path(results, "north_america", resultRaster))
 north_america <- exp(north_america) 
 west_asia <- raster(file.path(results, "west_asia", resultRaster))
 west_asia <- exp(west_asia)
-
-hist(africa)
-hist(asia)
-hist(europe)
-hist(latin_america)
-hist(north_america)
-hist(west_asia)
+# 
+# hist(africa)
+# hist(asia)
+# hist(europe)
+# hist(latin_america)
+# hist(north_america)
+# hist(west_asia)
 
 
 minV <-min(c(minValue(africa),
@@ -77,20 +84,6 @@ maxV <-max(c(maxValue(africa),
              maxValue(north_america),
              maxValue(west_asia)))
 
-meanV <- mean(c(cellStats(africa, stat='mean', na.rm=TRUE, asSample=TRUE),
-                cellStats(asia, stat='mean', na.rm=TRUE, asSample=TRUE),
-                cellStats(europe, stat='mean', na.rm=TRUE, asSample=TRUE),
-                cellStats(latin_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                cellStats(north_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                cellStats(west_asia, stat='mean', na.rm=TRUE, asSample=TRUE)))
-
-sdV <- mean(c(cellStats(africa, stat='sd', na.rm=TRUE, asSample=TRUE),
-              cellStats(asia, stat='sd', na.rm=TRUE, asSample=TRUE),
-              cellStats(europe, stat='sd', na.rm=TRUE, asSample=TRUE),
-              cellStats(latin_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-              cellStats(north_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-              cellStats(west_asia, stat='sd', na.rm=TRUE, asSample=TRUE)))
-
 
 
 colbrks <-  c(minV, seq(1, 6, length.out = 198), maxV)
@@ -98,7 +91,7 @@ colbrks <-  c(minV, seq(1, 6, length.out = 198), maxV)
 r.cols <- magma(199)
 
 # png(file.path(figures, "Richness.png"),width=17.5,height=8.75,units="cm",res=resdpi)
-pdf(file.path(figures, "Richness.pdf"),width=(17.5/2),height=(8.75/2))
+pdf(file.path(figures, "Richness.pdf"),width= wide_inch, height= wide_inch/2, pointsize = point_size)
 nf <- layout(matrix(c(1,2), 2,1, byrow = TRUE), c(5, 1), c(5, 1))
 # layout.show(nf)
 par(mar=c(0.1,0.1,0.1,0.1))
@@ -117,16 +110,16 @@ image(west_asia, col=r.cols, add = TRUE, breaks=colbrks, xaxt="n", yaxt="n", yla
 plotrix::corner.label(label = "(b)", x = -1, y = 1, cex=0.1)
 
 ## Legend
-par(mar=c(1,13,1,13))
+par(mar=c(0.1,13,1,13))
 scale <- c(rep(magma(199)[1], times = 20), rep(magma(199), each = 2), rep(magma(199)[199], times = 20))
 b <- barplot(rep(1, 438), col = scale, border =scale, axes = FALSE )
 # b
 # abline(v = 20, col = "white")
 # abline(v = b[418], col = "black")
 
-mtext(1, at = b[20], cex = 1)
-mtext(6, at = b[418], cex = 1)
-mtext("Number of species", at = 250, cex = 0.5)
+mtext(1, at = b[20], cex = 0.8)
+mtext(6, at = b[418],cex = 0.8)
+mtext("Number of species", at = 250, cex = 0.8)
 dev.off()
 
 
@@ -156,13 +149,13 @@ north_america <- exp(north_america) - 1
 west_asia <- raster(file.path(results, "west_asia", resultRaster))
 west_asia <- exp(west_asia) - 1
 
-  
-hist(africa, ylim =c(1, 1000))
-hist(asia, ylim =c(1, 1000))
-hist(europe, xlim = c(0, 50), breaks = seq(minValue(europe),  maxValue(europe), by = 1))
-hist(latin_america, ylim =c(1, 1000))
-hist(north_america, ylim =c(1, 1000))
-hist(west_asia, ylim =c(1, 1000))
+#   
+# hist(africa, ylim =c(1, 1000))
+# hist(asia, ylim =c(1, 1000))
+# hist(europe, xlim = c(0, 50), breaks = seq(minValue(europe),  maxValue(europe), by = 1))
+# hist(latin_america, ylim =c(1, 1000))
+# hist(north_america, ylim =c(1, 1000))
+# hist(west_asia, ylim =c(1, 1000))
   
 minV <-min(c(minValue(africa),
              minValue(asia),
@@ -177,21 +170,7 @@ maxV <-max(c(maxValue(africa),
              maxValue(latin_america),
              maxValue(north_america),
              maxValue(west_asia)))
-
- meanV <- mean(c(cellStats(africa, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(asia, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(europe, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(latin_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(north_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(west_asia, stat='mean', na.rm=TRUE, asSample=TRUE)))
  
- sdV <- mean(c(cellStats(africa, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(asia, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(europe, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(latin_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(north_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(west_asia, stat='sd', na.rm=TRUE, asSample=TRUE)))
-# 
  
 
 colbrks <-  c(minV, seq(1, 150, length.out = 198), maxV)
@@ -199,7 +178,7 @@ colbrks <-  c(minV, seq(1, 150, length.out = 198), maxV)
 r.cols <- magma(199)
 
 #png(file.path(figures, "Biomass.png"),width=17.5,height=8.75,units="cm",res=resdpi)
-pdf(file.path(figures, "Biomass.pdf"),width=(17.5/2),height=(8.75/2))
+pdf(file.path(figures, "Biomass.pdf"),width= wide_inch, height= wide_inch/2, pointsize = point_size)
 
 nf <- layout(matrix(c(1,2), 2,1, byrow = TRUE), c(5, 1), c(5, 1))
 # layout.show(nf)
@@ -220,9 +199,9 @@ plotrix::corner.label(label = "(d)", x = -1, y = 1, cex = plotlabcex)
 par(mar=c(1,13,1,13))
 scale <- c(rep(magma(199)[1], times = 20), rep(magma(199), each = 2), rep(magma(199)[199], times = 20))
 b <- barplot(rep(1, 438), col = scale, border =scale, axes = FALSE )
-mtext("1", at =b[20], cex = 1)
-mtext("150", at = b[418], cex = 1)
-mtext(expression("Biomass (grams per" ~ m^{2} ~ ")"), at = 250, cex = 0.5)
+mtext("1", at =b[20], cex = 0.8)
+mtext("150", at = b[418], cex = 0.8)
+mtext(expression("Biomass (grams per" ~ m^{2} ~ ")"), at = 250, cex = 0.8)
 dev.off()
 
 
@@ -267,26 +246,12 @@ maxV <-max(c(maxValue(africa),
              maxValue(north_america),
              maxValue(west_asia)))
 
-hist(asia)
-hist(africa)
-hist(europe)
-hist(north_america)
-hist(latin_america)
-hist(west_asia)
-# 
- meanV <- mean(c(cellStats(africa, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(asia, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(europe, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(latin_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(north_america, stat='mean', na.rm=TRUE, asSample=TRUE),
-                 cellStats(west_asia, stat='mean', na.rm=TRUE, asSample=TRUE)))
- 
- sdV <- mean(c(cellStats(africa, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(asia, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(europe, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(latin_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(north_america, stat='sd', na.rm=TRUE, asSample=TRUE),
-               cellStats(west_asia, stat='sd', na.rm=TRUE, asSample=TRUE)))
+# hist(asia)
+# hist(africa)
+# hist(europe)
+# hist(north_america)
+# hist(latin_america)
+# hist(west_asia)
 
 
 
@@ -297,7 +262,7 @@ colbrks <-  c(minV, seq(5, 150, length.out = 198), maxV)
 r.cols <- magma(199)
 
 #png(file.path(figures, "Abundance.png"),width=17.5,height=8.75,units="cm",res=resdpi)
-pdf(file.path(figures, "Abundance.pdf"),width=(17.5/2),height=(8.75/2))
+pdf(file.path(figures, "Abundance.pdf"),width= wide_inch, height= wide_inch/2, pointsize = point_size)
 
 nf <- layout(matrix(c(1,2), 2,1, byrow = TRUE), c(5, 1), c(5, 1))
 # layout.show(nf)
@@ -319,9 +284,9 @@ plotrix::corner.label(label = "(c)", x = -1, y = 1, cex = plotlabcex)
 par(mar=c(1,13,1,13))
 scale <- c(rep(magma(199)[1], times = 20), rep(magma(199), each = 2), rep(magma(199)[199], times = 20))
 b <- barplot(rep(1, 438), col = scale, border =scale, axes = FALSE )
-mtext("5", at = b[20], cex = 1)
-mtext("150", at = b[418], cex = 1)
-mtext(expression("Abundance (individuals per" ~ m^{2} ~ ")"), at = 250, cex = 0.5)
+mtext("5", at = b[20], cex = 0.8)
+mtext("150", at = b[418], cex = 0.8)
+mtext(expression("Abundance (individuals per" ~ m^{2} ~ ")"), at = 250, cex = 0.8)
 
 dev.off()
 
